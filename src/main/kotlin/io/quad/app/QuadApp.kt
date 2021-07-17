78e3a7e898c34d193f981a68c1d9d099dc850b78
TheFruxz <cedricspitzer@outlook.de>
2021-07-17 16:27:00 +0200

    Resolved current issues

--- src/main/kotlin/io/quad/app/QuadApp.kt
+++ src/main/kotlin/io/quad/app/QuadApp.kt
@@ -1,11 +1,16 @@
 package io.quad.app
 
 import io.quad.library.structure.app.App
+import io.quad.library.structure.app.AppCache
 import io.quad.library.structure.app.AppCompanion
 
 class QuadApp : App() {
 
 	override val companion = Companion
+
+	override val appIdentity = "QUAD"
+	override val appBrand = "QUAD"
+	override val appCache = QuadCache
 
 	companion object : AppCompanion<QuadApp> {
 		override lateinit var instance: QuadApp
