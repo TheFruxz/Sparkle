aa8f31250f967183873d5ad59029bfdf07e1d5e8
TheFruxz <cedricspitzer@outlook.de>
2021-07-18 16:10:36 +0200

    Added QUAD tryCatcher

--- src/main/kotlin/io/quad/library/extension/Exception.kt
+++ src/main/kotlin/io/quad/library/extension/Exception.kt
@@ -1,12 +1,24 @@
 package io.quad.library.extension
 
-import java.lang.Exception
+import kotlin.Exception
+import kotlin.random.Random
 
 fun catchException(exception: Exception) {
+
+	val exceptionIdentity = Random.nextInt(10000, 99999)
 
 	debugLog("caught exception: '${exception.localizedMessage}'")
 	exception.printStackTrace()
 
 	// TODO: 11.07.2021 Nice error message
 
+}
+
+fun quadTry(catchBlock: () -> Unit = { }, tryBlock: () -> Unit) {
+	try {
+		tryBlock()
+	} catch (e: Exception) {
+		catchBlock()
+		catchException(e)
+	}
 }
