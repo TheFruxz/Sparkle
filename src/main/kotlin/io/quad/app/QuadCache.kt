5c08511b1c97fa833c0f33c8e9faf49969a1b069
TheFruxz <cedricspitzer@outlook.de>
2021-07-18 16:11:28 +0200

    Implemented SandBox registrations

--- src/main/kotlin/io/quad/app/QuadCache.kt
+++ src/main/kotlin/io/quad/app/QuadCache.kt
@@ -1,5 +1,6 @@
 package io.quad.app
 
+import io.quad.library.runtime.sandbox.SandBox
 import io.quad.library.structure.app.App
 import io.quad.library.structure.app.AppCache
 import io.quad.library.structure.app.cache.CacheDepthLevel
@@ -8,6 +9,8 @@ import java.util.*
 object QuadCache : AppCache {
 
 	val registeredApplications = mutableSetOf<App>()
+
+	val registeredSandBoxes = mutableMapOf<String, SandBox>()
 
 	override fun dropEntityData(entityIdentity: UUID, dropDepth: CacheDepthLevel) {
 		TODO("Not yet implemented")
--- src/main/kotlin/io/quad/library/structure/app/App.kt
+++ src/main/kotlin/io/quad/library/structure/app/App.kt
@@ -5,11 +5,15 @@ package io.quad.library.structure.app
 import com.destroystokyo.paper.utils.PaperPluginLogger
 import io.quad.app.QuadCache
 import io.quad.library.extension.catchException
+import io.quad.library.extension.quadTry
 import io.quad.library.runtime.app.RunStatus
-import io.quad.library.runtime.app.RunStatus.OFFLINE
+import io.quad.library.runtime.app.RunStatus.*
+import io.quad.library.runtime.sandbox.SandBox
 import io.quad.library.structure.app.event.EventListener
 import io.quad.library.structure.command.Interchange
+import org.bukkit.configuration.file.YamlConfiguration
 import org.bukkit.plugin.java.JavaPlugin
+import java.io.InputStreamReader
 import java.util.logging.Level
 
 abstract class App : JavaPlugin() {
@@ -30,7 +34,7 @@ abstract class App : JavaPlugin() {
 	/**
 	 * The displayName
 	 */
-	abstract val appBrand: String
+	abstract val appLabel: String
 
 	/**
 	 * The cache of the application
@@ -65,6 +69,17 @@ abstract class App : JavaPlugin() {
 			log.log(Level.WARNING, "skipped registering '${eventListener.listenerIdentity}' listener, app disabled!")
 	}
 
+	fun add(sandBox: SandBox) {
+		QuadCache.registeredSandBoxes.put(
+			"${sandBox.sandBoxVendor.appIdentity}//${sandBox.sandBoxIdentity}",
+			sandBox
+		)
+		log.log(
+			Level.INFO,
+			"registered FUSION sandbox '${sandBox.sandBoxIdentity}'!"
+		)
+	}
+
 	// runtime
 
 	/**
@@ -73,8 +88,58 @@ abstract class App : JavaPlugin() {
 	var runStatus: RunStatus = OFFLINE
 		private set
 
-	val log = PaperPluginLogger.getLogger("QUAD/$appIdentity ($appBrand)")
+	var appRegistrationFile = YamlConfiguration()
+
+	val log = PaperPluginLogger.getLogger("QUAD/$appIdentity ($appLabel)")
 
 	protected val pluginManager = server.pluginManager
+
+	// override base-mechanics
+
+	abstract fun login()
+	abstract fun boot()
+	abstract fun logout()
+
+	/**
+	 * ***DO NOT OVERRIDE!***
+	 */
+	override fun onLoad() {
+		quadTry {
+
+			runStatus = PRE_LOAD
+			classLoader.getResourceAsStream("plugin.yml")?.let { resource ->
+				appRegistrationFile.load(InputStreamReader(resource))
+			}
+			login()
+			runStatus = LOAD
+
+		}
+	}
+
+	/**
+	 * ***DO NOT OVERRIDE!***
+	 */
+	override fun onEnable() {
+		quadTry {
+
+			runStatus = PRE_ENABLE
+			boot()
+			runStatus = ENABLE
+
+		}
+	}
+
+	/**
+	 * ***DO NOT OVERRIDE!***
+	 */
+	override fun onDisable() {
+		quadTry {
+
+			runStatus = SHUTDOWN
+			logout()
+			runStatus = OFFLINE
+
+		}
+	}
 
 }
