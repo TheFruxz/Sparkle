974fffd09b6c9a1d347fdaf3b9305531b08425cc
TheFruxz <cedricspitzer@outlook.de>
2021-08-01 15:40:57 +0200

    Improved smart task creation

--- src/main/kotlin/de/jet/library/extension/tasky/Task.kt
+++ src/main/kotlin/de/jet/library/extension/tasky/Task.kt
@@ -1,12 +1,28 @@
 package de.jet.library.extension.tasky
 
+import de.jet.library.JET
 import de.jet.library.structure.app.App
 import de.jet.library.tool.tasky.Tasky
 import de.jet.library.tool.tasky.TemporalAdvice
+import de.jet.library.tool.tasky.TemporalAdvice.Companion
 
 fun task(
 	vendor: App,
 	temporalAdvice: TemporalAdvice,
+	killAtError: Boolean = true,
+	process: Tasky.() -> Unit,
+) = Tasky.task(vendor, temporalAdvice, killAtError, process)
+
+fun sync(
+	vendor: App = JET.appInstance,
+	temporalAdvice: TemporalAdvice = TemporalAdvice.instant(async = false),
+	killAtError: Boolean = true,
+	process: Tasky.() -> Unit,
+) = Tasky.task(vendor, temporalAdvice, killAtError, process)
+
+fun async(
+	vendor: App = JET.appInstance,
+	temporalAdvice: TemporalAdvice = TemporalAdvice.instant(async = true),
 	killAtError: Boolean = true,
 	process: Tasky.() -> Unit,
 ) = Tasky.task(vendor, temporalAdvice, killAtError, process)
