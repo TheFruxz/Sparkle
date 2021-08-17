f0b549d7b9310e87774caf43931b75a6e2160d28
TheFruxz <cedricspitzer@outlook.de>
2021-08-17 12:00:31 +0200

    Added simple location data transformer

--- src/main/kotlin/de/jet/library/tool/data/DataTransformer.kt
+++ src/main/kotlin/de/jet/library/tool/data/DataTransformer.kt
@@ -1,5 +1,9 @@
 package de.jet.library.tool.data
 
+import de.jet.library.tool.display.world.SimpleLocation
+import de.jet.library.tool.display.world.SimpleLocation.Companion
+import org.bukkit.Location
+
 data class DataTransformer<SHELL: Any, CORE: Any>(
 	val toCore: SHELL.() -> CORE,
 	val toShell: CORE.() -> SHELL,
@@ -12,6 +16,9 @@ data class DataTransformer<SHELL: Any, CORE: Any>(
 
 		fun simpleColorCode() =
 			DataTransformer<String, String>({ replace("§", "COLOR>") }, { replace("COLOR>", "§") })
+
+		fun simpleLocationBukkit() =
+			DataTransformer<Location, SimpleLocation>({ SimpleLocation.ofBukkit(this) }, { bukkit })
 
 	}
 
