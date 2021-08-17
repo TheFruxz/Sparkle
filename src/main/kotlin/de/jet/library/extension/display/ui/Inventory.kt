a387a03dea8793694a248249f6a76319c5147c4a
TheFruxz <cedricspitzer@outlook.de>
2021-08-17 12:47:16 +0200

    Added PlayerInventory get/set operator functions

--- src/main/kotlin/de/jet/library/extension/display/ui/Inventory.kt
+++ src/main/kotlin/de/jet/library/extension/display/ui/Inventory.kt
@@ -7,6 +7,7 @@ import net.kyori.adventure.text.Component
 import org.bukkit.Bukkit
 import org.bukkit.inventory.Inventory
 import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.PlayerInventory
 
 fun buildContainer(lines: Int = 3, action: Container.() -> Unit) = Container(size = lines*9).apply(action)
 
@@ -22,3 +23,9 @@ operator fun Inventory.get(slot: Int) = getItem(slot)
 operator fun Inventory.set(slot: Int, itemStack: ItemStack) = setItem(slot, itemStack)
 
 operator fun Inventory.set(slot: Int, item: Item) = setItem(slot, item.produce())
+
+operator fun PlayerInventory.get(slot: Int) = getItem(slot)
+
+operator fun PlayerInventory.set(slot: Int, itemStack: ItemStack) = setItem(slot, itemStack)
+
+operator fun PlayerInventory.set(slot: Int, item: Item) = setItem(slot, item.produce())
