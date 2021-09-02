a7b7270c98dfe25230e1b46c385478ca8970e009
TheFruxz <cedricspitzer@outlook.de>
2021-09-02 18:48:09 +0200

    identityObject now added to player

--- src/main/kotlin/de/jet/library/extension/paper/Player.kt
+++ src/main/kotlin/de/jet/library/extension/paper/Player.kt
@@ -1,8 +1,10 @@
 package de.jet.library.extension.paper
 
 import de.jet.library.tool.permission.Approval
+import de.jet.library.tool.smart.Identity
 import org.bukkit.attribute.Attribute
 import org.bukkit.entity.LivingEntity
+import org.bukkit.entity.Player
 import org.bukkit.permissions.Permissible
 
 fun Permissible.hasApproval(approval: Approval) =
@@ -18,5 +20,8 @@ var LivingEntity.quickMaxHealth: Double
 fun LivingEntity.maxOutHealth() {
 	health = quickMaxHealth
 }
+
+val Player.identityObject: Identity<Player>
+	get() = Identity("$uniqueId")
 
 // TODO: 22.08.2021 Cooldowns here
