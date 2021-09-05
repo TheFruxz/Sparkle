00a71f93d8eb8ea11b95cdab1656b35c2518e8fc
TheFruxz <cedricspitzer@outlook.de>
2021-09-05 15:03:32 +0200

    Added Chat-Component

--- JET-Minecraft/src/main/kotlin/de/jet/minecraft/app/component/chat/JetChatComponent.kt
+++ JET-Minecraft/src/main/kotlin/de/jet/minecraft/app/component/chat/JetChatComponent.kt
@@ -1,14 +1,25 @@
 package de.jet.minecraft.app.component.chat
 
+import de.jet.library.extension.paper.getPlayer
+import de.jet.library.extension.paper.onlinePlayers
+import de.jet.minecraft.extension.display.buildTextComponent
+import de.jet.minecraft.extension.paper.legacyString
 import de.jet.minecraft.extension.system
 import de.jet.minecraft.structure.app.App
 import de.jet.minecraft.structure.app.event.EventListener
 import de.jet.minecraft.structure.component.Component
-import de.jet.minecraft.structure.component.Component.RunType.DISABLED
+import de.jet.minecraft.structure.component.Component.RunType.AUTOSTART_MUTABLE
 import io.papermc.paper.event.player.AsyncChatEvent
+import net.kyori.adventure.sound.Sound
+import net.kyori.adventure.sound.Sound.Source.MASTER
+import net.kyori.adventure.text.TextComponent
+import net.kyori.adventure.text.format.NamedTextColor
+import org.bukkit.Sound.BLOCK_NOTE_BLOCK_PLING
 import org.bukkit.event.EventHandler
+import java.util.*
+import net.kyori.adventure.text.Component as AdventureComponent
 
-internal class JetChatComponent(vendor: App = system) : Component(vendor, DISABLED /* TODO: 30.08.2021 Because has no contents! */ ) {
+internal class JetChatComponent(vendor: App = system) : Component(vendor, AUTOSTART_MUTABLE) {
 
 	override val thisIdentity = "Chat"
 
@@ -28,7 +39,53 @@ internal class JetChatComponent(vendor: App = system) : Component(vendor, DISABL
 
 		@EventHandler
 		fun onChat(event: AsyncChatEvent) {
-			TODO("$event would be used in the future!")
+			val player = event.player
+
+			val notify = mutableSetOf<UUID>()
+
+			val message: TextComponent = buildTextComponent {
+				event.message().legacyString.split(" ").forEach { snipped ->
+					val tagged = getPlayer(snipped.removePrefix("@"))
+
+					append(AdventureComponent.text(" "))
+
+					if (snipped.startsWith("@") && snipped.length > 2 && tagged != null && !notify.contains(tagged.uniqueId)) {
+						append(AdventureComponent.text("@${tagged.displayName().legacyString}", NamedTextColor.YELLOW))
+						notify.add(tagged.uniqueId)
+					} else if (snipped.startsWith("#") && snipped.length > 1) {
+						append(AdventureComponent.text(snipped, NamedTextColor.YELLOW))
+					} else if (snipped.equals("[item]", true)) {
+						append(player.inventory.itemInMainHand.displayName().hoverEvent(player.inventory.itemInMainHand).color(NamedTextColor.GRAY))
+					} else
+						append(AdventureComponent.text(snipped, NamedTextColor.GRAY))
+
+				}
+			}
+
+			val sender: TextComponent = buildTextComponent {
+				append(player.displayName().color(NamedTextColor.GRAY))
+			}
+
+			buildTextComponent {
+
+				append(AdventureComponent.text("▶ ", NamedTextColor.DARK_GRAY))
+				append(sender)
+				append(AdventureComponent.text(" |", NamedTextColor.DARK_GRAY))
+				append(message)
+
+			}.let { result ->
+				onlinePlayers.forEach { receiver ->
+
+					receiver.sendMessage(result)
+
+					if (notify.contains(receiver.uniqueId))
+						receiver.playSound(Sound.sound(BLOCK_NOTE_BLOCK_PLING, MASTER, 1F, 2F))
+
+				}
+			}
+
+			event.isCancelled = true
+
 		}
 
 	}
