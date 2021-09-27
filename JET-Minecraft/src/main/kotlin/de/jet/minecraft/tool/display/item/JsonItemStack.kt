7ff4f987a9d7e66c12faa363c0135f38e5e52ca4
TheFruxz <cedricspitzer@outlook.de>
2021-09-27 16:57:21 +0200

    < FIX > No warning of external file

--- JET-Minecraft/src/main/kotlin/de/jet/minecraft/tool/display/item/JsonItemStack.kt
+++ JET-Minecraft/src/main/kotlin/de/jet/minecraft/tool/display/item/JsonItemStack.kt
@@ -1,3 +1,5 @@
+@file:Suppress("DEPRECATION", "SENSELESS_COMPARISON", "DuplicatedCode", "ReplaceNegatedIsEmptyWithIsNotEmpty")
+
 import com.google.gson.Gson
 import com.google.gson.JsonArray
 import com.google.gson.JsonElement
