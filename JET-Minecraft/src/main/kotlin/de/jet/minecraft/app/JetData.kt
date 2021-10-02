46f07e19b4c08af55e4c867c8a44038f04185750
TheFruxz <cedricspitzer@outlook.de>
2021-10-02 21:28:40 +0200

    Removed item testing preference

--- JET-Minecraft/src/main/kotlin/de/jet/minecraft/app/JetData.kt
+++ JET-Minecraft/src/main/kotlin/de/jet/minecraft/app/JetData.kt
@@ -3,16 +3,9 @@ package de.jet.minecraft.app
 import de.jet.library.extension.data.jetPath
 import de.jet.minecraft.app.JetData.File.BRAIN
 import de.jet.minecraft.app.JetData.File.CONFIG
-import de.jet.minecraft.app.JetData.File.TESTING
-import de.jet.minecraft.extension.display.ui.item
 import de.jet.minecraft.tool.data.DataTransformer
 import de.jet.minecraft.tool.data.JetFile
 import de.jet.minecraft.tool.data.Preference
-import de.jet.minecraft.tool.display.item.Modification
-import org.bukkit.Material
-import org.bukkit.enchantments.Enchantment
-import org.bukkit.inventory.ItemFlag.HIDE_ATTRIBUTES
-import org.bukkit.inventory.ItemFlag.HIDE_ENCHANTS
 
 object JetData {
 
@@ -47,22 +40,6 @@ object JetData {
 		path = jetPath("touchedComponents"),
 		default = setOf<String>()
 	).transformer(DataTransformer.setCollection())
-
-	val item = Preference(
-		file = TESTING,
-		path = jetPath("item"),
-		default = Material.GRAVEL.item.apply {
-			label = "test"
-			lore = """
-				test1
-				test2
-				test3
-				test4
-			""".trimIndent()
-			annexModifications(Modification(Enchantment.FIRE_ASPECT, 20), Modification(Enchantment.DAMAGE_ALL, 5), Modification(Enchantment.SWEEPING_EDGE, 2))
-			annexFlags(HIDE_ATTRIBUTES, HIDE_ENCHANTS)
-		}
-	).transformer(DataTransformer.jsonItem())
 
 	object File {
 
