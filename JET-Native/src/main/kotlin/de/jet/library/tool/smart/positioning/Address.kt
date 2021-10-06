70a5aadf793897676a4dc0ad00444a6dd3bd1b3b
TheFruxz <cedricspitzer@outlook.de>
2021-10-06 12:13:21 +0200

    Improved Addresses

--- JET-Native/src/main/kotlin/de/jet/library/tool/smart/positioning/Address.kt
+++ JET-Native/src/main/kotlin/de/jet/library/tool/smart/positioning/Address.kt
@@ -1,22 +1,15 @@
 package de.jet.library.tool.smart.positioning
 
 import de.jet.library.tool.smart.identification.Identifiable
-import java.net.URL
 
-data class Address internal constructor(val address: String) : Identifiable<Address> {
+data class Address<T> internal constructor(val address: String) : Identifiable<Address<T>> {
 
     override val identity = address
 
     companion object {
 
-        fun address(path: String) =
-            Address(path)
-
-        fun address(url: URL) =
-            Address("$url")
-
-        fun address(identifiable: Identifiable<*>) =
-            Address(identifiable.identity)
+        fun <T> address(path: String) =
+            Address<T>(path)
 
     }
 
