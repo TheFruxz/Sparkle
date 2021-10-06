ddc0fba4efe4e7b55d4b6549426d865ecf541009
TheFruxz <cedricspitzer@outlook.de>
2021-10-06 10:06:24 +0200

    Added some placeholder messages
    Before the correct translated message will be inserted

--- JET-Minecraft/src/main/kotlin/de/jet/minecraft/app/interchange/PreferenceInterchange.kt
+++ JET-Minecraft/src/main/kotlin/de/jet/minecraft/app/interchange/PreferenceInterchange.kt
@@ -1,6 +1,7 @@
 package de.jet.minecraft.app.interchange
 
 import de.jet.minecraft.app.JetCache
+import de.jet.minecraft.extension.display.notification
 import de.jet.minecraft.extension.system
 import de.jet.minecraft.structure.app.App
 import de.jet.minecraft.structure.command.CompletionVariable
@@ -10,6 +11,9 @@ import de.jet.minecraft.structure.command.buildCompletion
 import de.jet.minecraft.structure.command.isRequired
 import de.jet.minecraft.structure.command.mustMatchOutput
 import de.jet.minecraft.structure.command.next
+import de.jet.minecraft.tool.display.message.Transmission
+import de.jet.minecraft.tool.display.message.Transmission.Level.APPLIED
+import de.jet.minecraft.tool.display.message.Transmission.Level.FAIL
 
 class PreferenceInterchange(vendor: App = system) : Interchange(vendor, "preference", requiresAuthorization = true, completion = buildCompletion {
 	next("list", "reset", "info") isRequired true mustMatchOutput true
@@ -28,14 +32,20 @@ class PreferenceInterchange(vendor: App = system) : Interchange(vendor, "prefere
 				if (preference != null) {
 					when (parameters.first()) {
 						"reset" -> {
+
 							preference.reset()
+
+							"Successfully reset the preference [preference] to its default value!"
+								.notification(APPLIED, executor).display()
+
 						}
 						"info" -> {
 
 						}
 					}
 				} else
-					TODO()
+					"No preference with the name [preference] currently registered!"
+						.notification(FAIL, executor).display()
 			}
 		}
 
