87e2741ba4f7516fadb4372eed72270c59aa95ae
TheFruxz <cedricspitzer@outlook.de>
2021-10-12 16:11:45 +0200

    Added more direct item-quirk api

--- JET-Minecraft/src/main/kotlin/de/jet/minecraft/tool/display/item/quirk/Quirk.kt
+++ JET-Minecraft/src/main/kotlin/de/jet/minecraft/tool/display/item/quirk/Quirk.kt
@@ -1,5 +1,6 @@
 package de.jet.minecraft.tool.display.item.quirk
 
+import de.jet.minecraft.tool.display.item.Item
 import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.meta.*
 
@@ -24,25 +25,47 @@ interface Quirk {
 
 		fun <T : ItemMeta> custom(process: T.() -> Unit) = processor(process)
 
+		fun <T : ItemMeta> Item.customQuirk(process: T.() -> Unit) = putQuirk(custom(process))
+
 		fun banner(process: BannerMeta.() -> Unit) = processor(process)
+
+		fun Item.bannerQuirk(process: BannerMeta.() -> Unit) = putQuirk(banner(process))
 
 		fun book(process: BookMeta.() -> Unit) = processor(process)
 
+		fun Item.bookQuirk(process: BookMeta.() -> Unit) = putQuirk(book(process))
+
 		fun compass(process: CompassMeta.() -> Unit) = processor(process)
+
+		fun Item.compassQuirk(process: CompassMeta.() -> Unit) = putQuirk(compass(process))
 
 		fun crossbow(process: CrossbowMeta.() -> Unit) = processor(process)
 
+		fun Item.crossbowQuirk(process: CrossbowMeta.() -> Unit) = putQuirk(crossbow(process))
+
 		fun firework(process: FireworkMeta.() -> Unit) = processor(process)
+
+		fun Item.fireworkQuirk(process: FireworkMeta.() -> Unit) = putQuirk(firework(process))
 
 		fun leatherArmor(process: LeatherArmorMeta.() -> Unit) = processor(process)
 
+		fun Item.leatherArmorQuirk(process: LeatherArmorMeta.() -> Unit) = putQuirk(leatherArmor(process))
+
 		fun map(process: MapMeta.() -> Unit) = processor(process)
+
+		fun Item.mapQuirk(process: MapMeta.() -> Unit) = putQuirk(map(process))
 
 		fun skull(process: SkullMeta.() -> Unit) = processor(process)
 
+		fun Item.skullQuirk(process: SkullMeta.() -> Unit) = putQuirk(skull(process))
+
 		fun potion(process: PotionMeta.() -> Unit) = processor(process)
 
+		fun Item.potionQuirk(process: PotionMeta.() -> Unit) = putQuirk(potion(process))
+
 		fun spawnEgg(process: SpawnEggMeta.() -> Unit) = processor(process)
+
+		fun Item.spawnEggQuirk(process: SpawnEggMeta.() -> Unit) = putQuirk(spawnEgg(process))
 
 	}
 
