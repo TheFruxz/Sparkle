85f856d00ac1e05ba53e08a44c26b93d32f146fa
TheFruxz <cedricspitzer@outlook.de>
2021-10-15 17:51:49 +0200

    Updated to Java 17

--- JET-Minecraft/build.gradle.kts
+++ JET-Minecraft/build.gradle.kts
@@ -1,4 +1,4 @@
-import org.gradle.api.JavaVersion.VERSION_16
+import org.gradle.api.JavaVersion.VERSION_17
 
 plugins {
     kotlin("jvm")
@@ -44,7 +44,7 @@ dependencies {
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions.jvmTarget = "16"
+    kotlinOptions.jvmTarget = "17"
     kotlinOptions.freeCompilerArgs += "-Xunrestricted-builder-inference"
     kotlinOptions.freeCompilerArgs += "-Xopt-in=kotlin.time.ExperimentalTime"
     kotlinOptions.freeCompilerArgs += "-Xopt-in=kotlin.ExperimentalStdlibApi"
@@ -53,8 +53,8 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
 }
 
 java {
-    sourceCompatibility = VERSION_16
-    targetCompatibility = VERSION_16
+    sourceCompatibility = VERSION_17
+    targetCompatibility = VERSION_17
 }
 
 tasks.processResources {
