86eade515ee1e67c8d971b117c5df793bd07d823
TheFruxz <cedricspitzer@outlook.de>
2021-10-24 15:47:34 +0200

    Added quick base64 converter

---
+++ JET-Native/src/main/kotlin/de/jet/library/tool/conversion/Base64.kt
@@ -0,0 +1,17 @@
+package de.jet.library.tool.conversion
+
+import java.util.Base64
+
+object Base64 {
+
+	/**
+	 * From String to Base64
+	 */
+	fun encodeToString(input: String): String = Base64.getEncoder().encodeToString(input.encodeToByteArray())
+
+	/**
+	 * From Base64 to String
+	 */
+	fun decodeToString(input: String): String = Base64.getDecoder().decode(input.encodeToByteArray()).decodeToString()
+
+}
