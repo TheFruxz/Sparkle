309a7ed0ec0c51c9f850f80dd5061d087a915bc9
TheFruxz <cedricspitzer@outlook.de>
2021-11-05 17:01:53 +0100

    Renamed from kotlin to JET, to avoid distraction

--- JET-Native/src/main/kotlin/de/jet/library/extension/timing/Calendar.kt
+++ JET-Native/src/main/kotlin/de/jet/library/extension/timing/Calendar.kt
@@ -3,7 +3,7 @@ package de.jet.library.extension.timing
 import de.jet.library.tool.timing.calendar.Calendar
 import java.util.Calendar as JavaUtilCalendar
 
-val JavaUtilCalendar.kotlinCalendar: Calendar
+val JavaUtilCalendar.jetCalendar: Calendar
 	get() = Calendar.fromLegacy(this)
 
-fun JavaUtilCalendar.editInKotlinEnvironment(action: Calendar.() -> Unit) = kotlinCalendar.apply(action).produce()
+fun JavaUtilCalendar.editInJETEnvironment(action: Calendar.() -> Unit) = jetCalendar.apply(action).produce()
