722face950eca2c5490e382092af18eeda57d02d
TheFruxz <cedricspitzer@outlook.de>
2021-11-07 14:31:38 +0100

    Removed computational values, that PaperMC added after JET

--- JET-Minecraft/src/main/kotlin/de/jet/minecraft/extension/paper/Event.kt
+++ JET-Minecraft/src/main/kotlin/de/jet/minecraft/extension/paper/Event.kt
@@ -1,16 +1,10 @@
-package de.jet.library.extension.paper
+package de.jet.minecraft.extension.paper
 
 import de.jet.minecraft.runtime.event.interact.PlayerInteractAtBlockEvent
 import org.bukkit.Location
 import org.bukkit.event.block.Action
 import org.bukkit.event.block.Action.*
 import org.bukkit.event.player.PlayerInteractEvent
-
-val Action.isRightClick: Boolean
-	get() = arrayOf(RIGHT_CLICK_AIR, RIGHT_CLICK_BLOCK).contains(this)
-
-val Action.isLeftClick: Boolean
-	get() = arrayOf(LEFT_CLICK_AIR, LEFT_CLICK_BLOCK).contains(this)
 
 val Action.isPhysical: Boolean
 	get() = this == PHYSICAL
