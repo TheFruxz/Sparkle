f0273b996a78989a12ccb58c88f817159c84c869
TheFruxz <cedricspitzer@outlook.de>
2021-11-15 21:05:40 +0100

    Updated dependencies & plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 import org.gradle.api.JavaVersion.VERSION_16
 
 plugins {
-    kotlin("jvm") version "1.5.31"
-    kotlin("plugin.serialization") version "1.5.31"
+    kotlin("jvm") version "1.6.0"
+    kotlin("plugin.serialization") version "1.6.0"
     id("org.jetbrains.dokka") version "1.5.31"
     id("com.github.johnrengelman.shadow") version "7.1.0"
-    id("org.jetbrains.qodana") version "0.1.12"
+    id("org.jetbrains.qodana") version "0.1.13"
     id("maven-publish")
 }
 
