7ef0b6dce0a90ab32e23cb37b990be2c25fb4982
Fruxz <cedricspitzer@outlook.de>
2021-11-23 13:02:45 +0100

    Fix: getResource...() Exception
    Fix an issue with the getResource functions, that the result can be not existant

--- JET-Native/src/main/kotlin/de/jet/library/extension/File.kt
+++ JET-Native/src/main/kotlin/de/jet/library/extension/File.kt
@@ -3,7 +3,7 @@
 package de.jet.library.extension
 
 inline fun getResourceText(resource: String) =
-    object {}.javaClass.getResource(resource)?.readText() ?: throw NoSuchElementException("Resource $resource not found")
+    object {}.javaClass.classLoader.getResource(resource)?.readText() ?: throw NoSuchElementException("Resource $resource not found")
 
 inline fun getResourceByteArray(resource: String) =
-    object {}.javaClass.getResource(resource)?.readBytes() ?: throw NoSuchElementException("Resource $resource not found")
+    object {}.javaClass.classLoader.getResource(resource)?.readBytes() ?: throw NoSuchElementException("Resource $resource not found")
