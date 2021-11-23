1f8e454f2808ffb8fff9edbacabe7e4a36dcd695
TheFruxz <cedricspitzer@outlook.de>
2021-11-23 17:56:30 +0100

    Feature: buildDiscordApi function

--- JET-Discord/src/main/kotlin/de/jet/discord/extension/Builder.kt
+++ JET-Discord/src/main/kotlin/de/jet/discord/extension/Builder.kt
@@ -1,7 +1,10 @@
 package de.jet.discord.extension
 
+import org.javacord.api.DiscordApiBuilder
 import org.javacord.api.entity.message.MessageBuilder
 import org.javacord.api.entity.message.embed.EmbedBuilder
+
+fun buildDiscordApi(process: DiscordApiBuilder.() -> Unit) = DiscordApiBuilder().apply(process)
 
 fun buildEmbed(process: EmbedBuilder.() -> Unit) = EmbedBuilder().apply(process)
 
