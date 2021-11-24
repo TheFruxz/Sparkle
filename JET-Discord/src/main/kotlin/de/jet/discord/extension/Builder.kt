2728fe0eb774f6805baa71c4f706c24344375359
TheFruxz <cedricspitzer@outlook.de>
2021-11-24 14:19:48 +0100

    Documentation: Add Builder.kt documentation

--- JET-Discord/src/main/kotlin/de/jet/discord/extension/Builder.kt
+++ JET-Discord/src/main/kotlin/de/jet/discord/extension/Builder.kt
@@ -4,8 +4,26 @@ import org.javacord.api.DiscordApiBuilder
 import org.javacord.api.entity.message.MessageBuilder
 import org.javacord.api.entity.message.embed.EmbedBuilder
 
+/**
+ * Builder for [DiscordApiBuilder]
+ * @param process modification of the [DiscordApiBuilder]
+ * @author Fruxz
+ * @since 1.0
+ */
 fun buildDiscordApi(process: DiscordApiBuilder.() -> Unit) = DiscordApiBuilder().apply(process)
 
+/**
+ * Builder for [EmbedBuilder]
+ * @param process modification of the [EmbedBuilder]
+ * @author Fruxz
+ * @since 1.0
+ */
 fun buildEmbed(process: EmbedBuilder.() -> Unit) = EmbedBuilder().apply(process)
 
+/**
+ * Builder for [MessageBuilder]
+ * @param process modification of the [MessageBuilder]
+ * @author Fruxz
+ * @since 1.0
+ */
 fun buildMessage(process: MessageBuilder.() -> Unit) = MessageBuilder().apply(process)
