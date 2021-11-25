b9fae4e224f69d5902c46b4a53f71745f38bff39
TheFruxz <cedricspitzer@outlook.de>
2021-11-25 17:31:28 +0100

    Documentation: add MapFunction.kt docs

--- JET-Native/src/main/kotlin/de/jet/library/extension/collection/MapFunction.kt
+++ JET-Native/src/main/kotlin/de/jet/library/extension/collection/MapFunction.kt
@@ -1,23 +1,96 @@
 package de.jet.library.extension.collection
 
+/**
+ * Returns the [Iterable]<[T]> as a [List] where
+ * every element is turned to a [String]. The
+ * backend is converting every element using
+ * the internal [toString] method.
+ * @param T the type of the original elements
+ * @return the list of [String]s
+ * @author Fruxz
+ * @since 1.0
+ */
 fun <T> Iterable<T>.mapToString() =
 	map { "$it" }
 
+/**
+ * Returns the [Iterable]<[T]> as a [List] where
+ * every element is turned to a [Int] with the
+ * [toInt] function, that is attached to a
+ * [String]-Object only containing the individuell
+ * content-entry of [this]
+ * @param T the type of the original elements
+ * @return the list of [Int]s
+ * @author Fruxz
+ * @since 1.0
+ */
 fun <T> Iterable<T>.mapToInt() =
 	map { "$it".toInt() }
 
+/**
+ * Returns the [Iterable]<[T]> as a [List] where
+ * every element is turned to a [Double] with the
+ * [toDouble] function, that is attached to a
+ * [String]-Object only containing the individuell
+ * content-entry of [this]
+ * @param T the type of the original elements
+ * @return the list of [Double]s
+ * @author Fruxz
+ * @since 1.0
+ */
 fun <T> Iterable<T>.mapToDouble() =
 	map { "$it".toDouble() }
 
+/**
+ * Returns the [Iterable]<[T]> as a [List] where
+ * every element is turned to a [Long] with the
+ * [toLong] function, that is attached to a
+ * [String]-Object only containing the individuell
+ * content-entry of [this]
+ * @param T the type of the original elements
+ * @return the list of [Long]s
+ * @author Fruxz
+ * @since 1.0
+ */
 fun <T> Iterable<T>.mapToLong() =
 	map { "$it".toLong() }
 
+/**
+ * Returns the [Iterable]<[T]> as a [List] where
+ * every element is turned to a [Byte] with the
+ * [toByte] function, that is attached to a
+ * [String]-Object only containing the individuell
+ * content-entry of [this]
+ * @param T the type of the original elements
+ * @return the list of [Byte]s
+ * @author Fruxz
+ * @since 1.0
+ */
 fun <T> Iterable<T>.mapToByte() =
 	map { "$it".toByte() }
 
+/**
+ * Returns the [Iterable]<[T]> as a [List] where
+ * every element is turned to a [Boolean] with the
+ * [toBoolean] function, that is attached to a
+ * [String]-Object only containing the individuell
+ * content-entry of [this]
+ * @param T the type of the original elements
+ * @return the list of [Boolean]s
+ * @author Fruxz
+ * @since 1.0
+ */
 fun <T> Iterable<T>.mapToBoolean() =
 	map { "$it".toBoolean() }
 
+/**
+ * Cast every element of the [Iterable]<[T]> to [O]
+ * @param T the type of the original elements
+ * @param O the type of the result elements
+ * @return the list of [O] elements
+ * @author Fruxz
+ * @since 1.0
+ */
 @Suppress("UNCHECKED_CAST")
 fun <T, O> Iterable<T>.mapCast() =
 	map { it as O }
