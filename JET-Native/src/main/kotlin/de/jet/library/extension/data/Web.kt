395798595e5bd47f3924e66c4d800f3875ac546c
TheFruxz <cedricspitzer@outlook.de>
2021-11-25 21:43:40 +0100

    Documentation: add Web.kt docs

--- JET-Native/src/main/kotlin/de/jet/library/extension/data/Web.kt
+++ JET-Native/src/main/kotlin/de/jet/library/extension/data/Web.kt
@@ -2,4 +2,11 @@ package de.jet.library.extension.data
 
 import java.net.URL
 
+/**
+ * Returns the text of the web page at the given [url].
+ * @param url the url of the web page, which will be downloaded
+ * @return the text of the web page
+ * @author Fruxz
+ * @since 1.0
+ */
 fun getWebText(url: String) = URL(url).openStream().bufferedReader().readText()
