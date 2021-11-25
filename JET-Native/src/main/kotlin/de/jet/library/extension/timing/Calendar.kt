ea162d68775559afe78b251d5acc2923728dda26
TheFruxz <cedricspitzer@outlook.de>
2021-11-25 22:03:12 +0100

    Documentation: add Calendar.kt docs

--- JET-Native/src/main/kotlin/de/jet/library/extension/timing/Calendar.kt
+++ JET-Native/src/main/kotlin/de/jet/library/extension/timing/Calendar.kt
@@ -3,7 +3,22 @@ package de.jet.library.extension.timing
 import de.jet.library.tool.timing.calendar.Calendar
 import java.util.Calendar as JavaUtilCalendar
 
+/**
+ * Converts a [JavaUtilCalendar] to a [Calendar] from JET, but keeps it contents.
+ * @author Fruxz
+ * @since 1.0
+ */
 val JavaUtilCalendar.jetCalendar: Calendar
 	get() = Calendar.fromLegacy(this)
 
+/**
+ * Gets the [JavaUtilCalendar], internally converts it, with its contents, to a
+ * JET-[Calendar], edit it with the [action] in the JET-Calender-Environment and
+ * returns the [Calendar] converted back to a [JavaUtilCalendar] with the new
+ * values containing inside the [JavaUtilCalendar].
+ * @param action the edit process, which is executed in the JET-[Calendar]-Environment
+ * @return the [JavaUtilCalendar] with the new values
+ * @author Fruxz
+ * @since 1.0
+ */
 fun JavaUtilCalendar.editInJETEnvironment(action: Calendar.() -> Unit) = jetCalendar.apply(action).produce()
