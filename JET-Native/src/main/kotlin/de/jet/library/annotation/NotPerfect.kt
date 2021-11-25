b1c8b13e04804bf69513598213a6908622fe2857
TheFruxz <cedricspitzer@outlook.de>
2021-11-25 15:16:47 +0100

    Fix NotPerfect.kt spelling

--- JET-Native/src/main/kotlin/de/jet/library/annotation/NotPerfect.kt
+++ JET-Native/src/main/kotlin/de/jet/library/annotation/NotPerfect.kt
@@ -1,7 +1,7 @@
 package de.jet.library.annotation
 
 /**
- * This annation marks every code that is not perfectly balanced, optimized or does not
+ * This annotation marks every code that is not perfectly balanced, optimized or does not
  * produce a perfectly balanced or optimized result.
  * @author Fruxz
  * @since 1.0
