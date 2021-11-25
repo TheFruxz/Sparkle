479bf79616d8de1e9542489b75cdfcf276e18169
TheFruxz <cedricspitzer@outlook.de>
2021-11-25 21:44:57 +0100

    Documentation: add Time.kt docs

--- JET-Native/src/main/kotlin/de/jet/library/extension/display/Time.kt
+++ JET-Native/src/main/kotlin/de/jet/library/extension/display/Time.kt
@@ -4,4 +4,10 @@ import de.jet.library.tool.timing.calendar.DisplayTime
 import de.jet.library.tool.timing.calendar.DisplayTime.Format.SECONDS
 import kotlin.time.Duration
 
+/**
+ * This function creates a [DisplayTime] from a [Duration] using its [Duration.inWholeSeconds] value.
+ * @return [DisplayTime]
+ * @author Fruxz
+ * @since 1.0
+ */
 fun Duration.display() = DisplayTime(SECONDS, this.inWholeSeconds.toDouble())
