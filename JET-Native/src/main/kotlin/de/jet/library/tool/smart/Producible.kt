9153f2c3af7b40793d191d5b413d69997c11e3dd
TheFruxz <cedricspitzer@outlook.de>
2021-11-26 13:21:54 +0100

    Documentation: add Producible.kt docs

--- JET-Native/src/main/kotlin/de/jet/library/tool/smart/Producible.kt
+++ JET-Native/src/main/kotlin/de/jet/library/tool/smart/Producible.kt
@@ -1,5 +1,10 @@
 package de.jet.library.tool.smart
 
+/**
+ * This interface marks every class/object, that can produce a object type [T]
+ * @author Fruxz
+ * @since 1.0
+ */
 interface Producible<T : Any> {
 
 	fun produce(): T
