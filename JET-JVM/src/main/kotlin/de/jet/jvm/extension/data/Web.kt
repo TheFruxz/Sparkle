a13c9bd5700f57940f7191d78178dcee74738076
TheFruxz <cedricspitzer@outlook.de>
2021-11-27 19:06:19 +0100

    Feature: add readWebBytes function

--- JET-JVM/src/main/kotlin/de/jet/jvm/extension/data/Web.kt
+++ JET-JVM/src/main/kotlin/de/jet/jvm/extension/data/Web.kt
@@ -10,3 +10,12 @@ import java.nio.charset.Charset
  * @since 1.0
  */
 fun getWebText(url: String, charset: Charset = Charsets.UTF_8) = url(url).readText(charset)
+
+/**
+ * Returns the content of the web page at the given [url].
+ * @param url the url of the web page, which will be downloaded
+ * @return the content of the web page as [ByteArray]
+ * @author Fruxz
+ * @since 1.0
+ */
+fun readWebBytes(url: String) = url(url).readBytes()
