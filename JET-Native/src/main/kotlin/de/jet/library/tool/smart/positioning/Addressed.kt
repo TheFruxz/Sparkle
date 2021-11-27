23096cdff88c72b4a9de72ae612494c9717521e7
TheFruxz <cedricspitzer@outlook.de>
2021-11-27 13:45:34 +0100

    Documentation: add Addressed docs

--- JET-Native/src/main/kotlin/de/jet/library/tool/smart/positioning/Addressed.kt
+++ JET-Native/src/main/kotlin/de/jet/library/tool/smart/positioning/Addressed.kt
@@ -2,6 +2,14 @@
 
 package de.jet.library.tool.smart.positioning
 
+/**
+ * A positioned element with an address.
+ * Kinda the same as a withIndex() in a list.
+ * @param address the address of the element
+ * @param value the value of the element
+ * @author Fruxz
+ * @since 1.0
+ */
 data class Addressed<T>(
     val address: Address<T>,
     val value: T,
