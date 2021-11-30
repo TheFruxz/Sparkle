7f8977a99311985442a9e14095b4fbd9b4b4059e
TheFruxz <cedricspitzer@outlook.de>
2021-11-30 22:31:02 +0100

    Feature: add basic bot creation

---
+++ JET-JavaCord/src/main/kotlin/de/jet/javacord/structure/Bot.kt
@@ -0,0 +1,26 @@
+package de.jet.javacord.structure
+
+import org.javacord.api.DiscordApi
+import org.javacord.api.DiscordApiBuilder
+
+/**
+ * This data class represents the configuration state of a discord-api bot.
+ * @param data the credentials of the bot to login with
+ * @param appearance the public appearance of the bot
+ * @param processPreProcess the process, before the bot-builder will be created
+ * @param processPreSetup the process, before the bot-builder will be setup
+ * @param processPreLogin the process, before the bot-builder will be logged in with the [data]
+ * @param processPostLogin the process, after the bot-builder has been logged in with the [data]
+ * @param processPostProcess the process, after every internal operation finished
+ * @author Fruxz
+ * @since 1.0
+ */
+data class Bot(
+	var data: BotCredentials,
+	var appearance: BotAppearance,
+	var processPreProcess: Bot.() -> Unit = { },
+	var processPreSetup: DiscordApiBuilder.() -> Unit = { },
+	var processPreLogin: DiscordApiBuilder.() -> Unit = { },
+	var processPostLogin: DiscordApi.() -> Unit = { },
+	var processPostProcess: DiscordApi.() -> Unit = { },
+)
--- JET-JavaCord/src/main/kotlin/de/jet/javacord/structure/BotData.kt
+++
@@ -1,5 +0,0 @@
-package de.jet.javacord.structure
-
-data class BotData(
-	val botToken: String,
-)
