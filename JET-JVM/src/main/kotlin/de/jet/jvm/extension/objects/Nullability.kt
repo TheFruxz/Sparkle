50f5de0190abd9181c3771099d82494a60249593
TheFruxz <cedricspitzer@outlook.de>
2021-12-09 11:22:33 +0100

    Feature: trustOrThrow() function

--- JET-JVM/src/main/kotlin/de/jet/jvm/extension/objects/Nullability.kt
+++ JET-JVM/src/main/kotlin/de/jet/jvm/extension/objects/Nullability.kt
@@ -4,18 +4,26 @@ import de.jet.jvm.extension.isNotNull
 import org.jetbrains.annotations.NotNull
 
 /**
+ * Converts a nullable object/value to a non-null object/value, if it is not null.
+ * If it is null it throws the [throwable].
+ * @author Fruxz
+ * @since 1.0
+ */
+fun <T : Any> T?.trustOrThrow(throwable: Throwable): T {
+	if (isNotNull) {
+		return this!!
+	} else
+		throw throwable
+}
+
+/**
  * Converts a nullable object/value to a non-null object/value, if it is not null, or
  * if it is null throw a [NoSuchElementException] instead
  * @throws NoSuchElementException
  */
 @Throws(NoSuchElementException::class)
 @NotNull
-fun <T : Any> T?.trust(): T {
-	if (isNotNull) {
-		return this!!
-	} else
-		throw NoSuchElementException("element [T] was null, but it is not allowed to be null!")
-}
+fun <T : Any> T?.trust() = trustOrThrow(NoSuchElementException("element [T] was null, but it is not allowed to be null!"))
 
 /**
  * Waits, until the element is not null, then returns the not-null it.
