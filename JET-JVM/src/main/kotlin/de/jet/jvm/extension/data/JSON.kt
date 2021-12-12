22d54dd73b9011cc09732c508a17c02231a59987
TheFruxz <cedricspitzer@outlook.de>
2021-12-12 17:18:36 +0100

    Fix: unflexible json
    Improved the flexibility and reliability of the json api

--- JET-JVM/src/main/kotlin/de/jet/jvm/extension/data/JSON.kt
+++ JET-JVM/src/main/kotlin/de/jet/jvm/extension/data/JSON.kt
@@ -4,6 +4,17 @@ import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
+val jsonBase = Json {
+	prettyPrint = true
+	isLenient = true
+	ignoreUnknownKeys = true
+	coerceInputValues = true
+	encodeDefaults = true
+	explicitNulls = true
+	allowStructuredMapKeys = true
+	allowSpecialFloatingPointValues = true
+}
+
 /**
  * Tries to encode the given object to a JSON string using the Kotlinx
  * serialization library's [Json.encodeToString] function.
@@ -11,7 +22,7 @@ import kotlinx.serialization.json.Json
  * @author Fruxz
  * @since 1.0
  */
-inline fun <reified T : Any> T.toJson() = Json.encodeToString(this)
+inline fun <reified T : Any> T.toJson() = jsonBase.encodeToString(this)
 
 /**
  * Tries to decode the given JSON string to an object type [T] using the
@@ -21,4 +32,4 @@ inline fun <reified T : Any> T.toJson() = Json.encodeToString(this)
  * @author Fruxz
  * @since 1.0
  */
-inline fun <reified T : Any> String.fromJson() = Json.decodeFromString<T>(this)
+inline fun <reified T : Any> String.fromJson() = jsonBase.decodeFromString<T>(this)
