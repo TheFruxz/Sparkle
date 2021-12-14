5a01c013446bbcf9b80c3aee0a9aee05e1303240
TheFruxz <cedricspitzer@outlook.de>
2021-12-14 21:51:28 +0100

    Chore(docs): complete InterchangeStructureBranch.kt documentation

--- JET-JVM/src/main/kotlin/de/jet/jvm/interchange/InterchangeStructureBranch.kt
+++ JET-JVM/src/main/kotlin/de/jet/jvm/interchange/InterchangeStructureBranch.kt
@@ -3,6 +3,15 @@ package de.jet.jvm.interchange
 import de.jet.jvm.extension.forceCast
 import de.jet.jvm.extension.forceCastOrNull
 
+/**
+ * This open class defines the functions and the structure of JVM Interchange
+ * branches and their behavior.
+ * @param branchName is the name of the branch
+ * @param path is their location inside the interchange structure
+ * @param branches are the sub-branches of this branch
+ * @author Fruxz
+ * @since 1.0
+ */
 open class InterchangeStructureBranch(
     open val branchName: String,
     open val path: String,
