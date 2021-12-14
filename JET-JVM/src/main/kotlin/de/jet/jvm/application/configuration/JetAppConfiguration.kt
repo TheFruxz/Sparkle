bdcc045fcc8d3c0f1ef47f7eeb1955513afad032
TheFruxz <cedricspitzer@outlook.de>
2021-12-14 16:13:52 +0100

    Chore(docs): add documentation of JetAppConfiguration.kt

--- JET-JVM/src/main/kotlin/de/jet/jvm/application/configuration/JetAppConfiguration.kt
+++ JET-JVM/src/main/kotlin/de/jet/jvm/application/configuration/JetAppConfiguration.kt
@@ -5,6 +5,16 @@ import de.jet.jvm.application.tag.version
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
+/**
+ * This data class represents the configuration of JET.
+ * This configuration contains the version of the JET,
+ * that created the configuration file. (compatibility)
+ * and also a list of [JetAppConfigModule]s.
+ * @param jetVersion the JET-instance, that created the configuration file.
+ * @param apps the list of [JetAppConfigModule]s, used/configured to run with JET.
+ * @author Fruxz
+ * @since 1.0
+ */
 @Serializable
 @SerialName("appConfiguration")
 data class JetAppConfiguration(
