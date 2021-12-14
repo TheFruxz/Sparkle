3504faed534526557bb9fe360c37eec9dba7342c
TheFruxz <cedricspitzer@outlook.de>
2021-12-14 21:06:58 +0100

    Chore(docs): add ConsoleInterchange.kt documentation

--- JET-JVM/src/main/kotlin/de/jet/jvm/extension/input/ConsoleInterchange.kt
+++ JET-JVM/src/main/kotlin/de/jet/jvm/extension/input/ConsoleInterchange.kt
@@ -5,12 +5,27 @@ package de.jet.jvm.extension.input
 import de.jet.jvm.application.console.interchange.ConsoleInterchange
 import de.jet.jvm.extension.collection.emptyString
 
+/**
+ * Constructs a new [ConsoleInterchange] using its [ConsoleInterchange.Builder].
+ * @param name of the future interchange
+ * @param process the modification of the [ConsoleInterchange.Builder] state
+ * @author Fruxz
+ * @since 1.0
+ */
 fun buildConsoleInterchange(name: String, process: ConsoleInterchange.Builder.() -> Unit = { }): ConsoleInterchange {
     val builder = ConsoleInterchange.Builder(name, emptyString())
     builder.apply(process)
     return builder.produce()
 }
 
+/**
+ * Requests an input from the user, like a terminal input,
+ * but only give the given [interchanges] as possible
+ * input and syntax.
+ * @param interchanges the possible input interchanges
+ * @author Fruxz
+ * @since 1.0
+ */
 fun requestTerminalInterchangeInput(vararg interchanges: ConsoleInterchange) {
     println(buildString {
         appendLine("Welcome to the custom JET JVM Console Interchange Console!")
