256f000a95608d3d51c9dee1c08dd1d1d2421431
TheFruxz <cedricspitzer@outlook.de>
2021-12-14 20:54:52 +0100

    Chore(docs): add Runtime.kt documentation

--- JET-JVM/src/main/kotlin/de/jet/jvm/extension/app/Runtime.kt
+++ JET-JVM/src/main/kotlin/de/jet/jvm/extension/app/Runtime.kt
@@ -5,6 +5,16 @@ import de.jet.jvm.application.runtime.JetAppRuntime
 import de.jet.jvm.application.tag.Version
 import de.jet.jvm.application.tag.version
 
+/**
+ * This function creates and runs a new application run
+ * @see JetApp
+ * @param appName is the name of the running application
+ * @param appVersion is the version of the running application
+ * @param appInstance is the [JetApp] on which the [JetAppRuntime] is based
+ * @param runtime is the process, which get [apply]'d after the initialization of the [JetAppRuntime]
+ * @author Fruxz
+ * @since 1.0
+ */
 fun runApp(appName: String, appVersion: Version = 1.0.version, appInstance: JetApp = JetApp(appName, appVersion), runtime: JetAppRuntime.() -> Unit) {
 	with(appInstance) { JetAppRuntime(this.identity, this.version) }.apply {
 		init()
