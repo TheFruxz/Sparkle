02fc2b5d799211d922e4297c2b6e570781ff6188
TheFruxz <cedricspitzer@outlook.de>
2021-12-14 08:52:04 +0100

    Chore(docs): add DiscordBotExtension documentation

--- JET-JavaCord/src/main/kotlin/de/jet/javacord/app/DiscordBotExtension.kt
+++ JET-JavaCord/src/main/kotlin/de/jet/javacord/app/DiscordBotExtension.kt
@@ -7,6 +7,16 @@ import de.jet.javacord.structure.BotCredentials
 import de.jet.jvm.application.extension.AppExtension
 import kotlin.reflect.KFunction1
 
+/**
+ * This is a [AppExtension], that runs a DiscordBot using the
+ * [runBot] function. It can be attached to a App-Runtime using
+ * the *attach* or *attachWith* function.
+ * @property identity is "discordbot"
+ * @property parallelRunAllowed is true - parallel runs allowed
+ * @property runtimeAccessor is [runBot] - also a builder for the bot instance
+ * @author Fruxz
+ * @since 1.0
+ */
 object DiscordBotExtension : AppExtension<Bot, Unit, Unit> {
 
 	override val identity = "discordbot"
