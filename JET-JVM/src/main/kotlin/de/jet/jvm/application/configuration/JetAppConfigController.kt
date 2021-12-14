098a572fa244235f85a204f0a803f24c1de420b0
TheFruxz <cedricspitzer@outlook.de>
2021-12-14 16:08:34 +0100

    Chore(docs): add documentation of JetAppConfigController.kt

--- JET-JVM/src/main/kotlin/de/jet/jvm/application/configuration/JetAppConfigController.kt
+++ JET-JVM/src/main/kotlin/de/jet/jvm/application/configuration/JetAppConfigController.kt
@@ -4,13 +4,34 @@ import de.jet.jvm.extension.data.fromJson
 import de.jet.jvm.extension.data.toJson
 import java.io.File
 
-
+/**
+ * This class is responsible for the configuration of applications.
+ * It manages, stores and loads the configuration of them.
+ * @author Fruxz
+ * @since 1.0
+ */
 object JetAppConfigController {
 
+	/**
+	 * The general configuration file of the jet management system itself
+	 * @author Fruxz
+	 * @since 1.0
+	 */
 	val jetConfig = File("jet.apps.json")
 
-	private lateinit var configState: JetAppConfiguration
+	/**
+	 * The current configuration state of the application
+	 * @author Fruxz
+	 * @since 1.0
+	 */
+	private lateinit var configState: JetAppConfiguration // TODO multi application support
 
+	/**
+	 * Gets the current or the loaded configuration state of the application
+	 * @return the current or saved state of the config
+	 * @author Fruxz
+	 * @since 1.0
+	 */
 	fun get(): JetAppConfiguration {
 		return if (JetAppConfigController::configState.isInitialized) {
 			configState
@@ -25,10 +46,23 @@ object JetAppConfigController {
 		}
 	}
 
+	/**
+	 * Changes the current configuration state of the application to [content]
+	 * @author Fruxz
+	 * @since 1.0
+	 */
 	fun set(content: JetAppConfiguration = JetAppConfiguration(apps = emptyList())) {
+		configState = content
 		jetConfig.writeText(content.toJson())
 	}
 
+	/**
+	 * This function adds an app to the configuration with the module
+	 * values, which often use the default values.
+	 * @param appModule the module of the app, to get saved
+	 * @author Fruxz
+	 * @since 1.0
+	 */
 	fun addApp(appModule: JetAppConfigModule) {
 		val get = get()
 		if (get.apps.none { it.identity == appModule.identity }) {
@@ -38,6 +72,16 @@ object JetAppConfigController {
 		}
 	}
 
+	/**
+	 * This functio returns the [JetAppConfigModule] of the searched
+	 * jet app [app] or returns null, if the module is not stored.
+	 * The module contains, most importantly, the configured path
+	 * of the files, which the [app] uses.
+	 * @param app is the searched app
+	 * @return the found module or null
+	 * @author Fruxz
+	 * @since 1.0
+	 */
 	fun getApp(app: JetApp): JetAppConfigModule? {
 		return get().apps.firstOrNull { it.identity == app.identity }
 	}
