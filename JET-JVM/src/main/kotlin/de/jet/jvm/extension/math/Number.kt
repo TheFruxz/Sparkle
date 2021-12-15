cc55df9ed07707851065d0989828171bcc555e2d
TheFruxz <cedricspitzer@outlook.de>
2021-12-15 16:05:55 +0100

    Chore(docs): complete Number.kt documentation

--- JET-JVM/src/main/kotlin/de/jet/jvm/extension/math/Number.kt
+++ JET-JVM/src/main/kotlin/de/jet/jvm/extension/math/Number.kt
@@ -104,86 +104,314 @@ fun Int.minedOut(minimum: Int) = minTo(minimum)
 
 // max functions
 
+/**
+ * This function limits [this] number to the [maximum] number.
+ * @param maximum the maximum size of the number.
+ * @author Fruxz
+ * @since 1.0
+ */
 fun Byte.maxTo(maximum: Byte) = let { if (it <= maximum) it else maximum }
 
+/**
+ * This function limits [this] number to the [maximum] number.
+ * @param maximum the maximum size of the number.
+ * @author Fruxz
+ * @since 1.0
+ */
 fun Short.maxTo(maximum: Short) = let { if (it <= maximum) it else maximum }
 
+/**
+ * This function limits [this] number to the [maximum] number.
+ * @param maximum the maximum size of the number.
+ * @author Fruxz
+ * @since 1.0
+ */
 fun Int.maxTo(maximum: Int) = let { if (it <= maximum) it else maximum }
 
+/**
+ * This function limits [this] number to the [maximum] number.
+ * @param maximum the maximum size of the number.
+ * @author Fruxz
+ * @since 1.0
+ */
 fun Long.maxTo(maximum: Long) = let { if (it <= maximum) it else maximum }
 
+/**
+ * This function limits [this] number to the [maximum] number.
+ * @param maximum the maximum size of the number.
+ * @author Fruxz
+ * @since 1.0
+ */
 fun Float.maxTo(maximum: Float) = let { if (it <= maximum) it else maximum }
 
+/**
+ * This function limits [this] number to the [maximum] number.
+ * @param maximum the maximum size of the number.
+ * @author Fruxz
+ * @since 1.0
+ */
 fun Double.maxTo(maximum: Double) = let { if (it <= maximum) it else maximum }
 
+/**
+ * This function limits [this] number to the [maximum] number.
+ * @param maximum the maximum size of the number.
+ * @author Fruxz
+ * @since 1.0
+ */
 fun UByte.maxTo(maximum: UByte) = let { if (it <= maximum) it else maximum }
 
+/**
+ * This function limits [this] number to the [maximum] number.
+ * @param maximum the maximum size of the number.
+ * @author Fruxz
+ * @since 1.0
+ */
 fun UShort.maxTo(maximum: UShort) = let { if (it <= maximum) it else maximum }
 
+/**
+ * This function limits [this] number to the [maximum] number.
+ * @param maximum the maximum size of the number.
+ * @author Fruxz
+ * @since 1.0
+ */
 fun UInt.maxTo(maximum: UInt) = let { if (it <= maximum) it else maximum }
 
+/**
+ * This function limits [this] number to the [maximum] number.
+ * @param maximum the maximum size of the number.
+ * @author Fruxz
+ * @since 1.0
+ */
 fun ULong.maxTo(maximum: ULong) = let { if (it <= maximum) it else maximum }
 
 // min functions
 
+/**
+ * This function limits [this] number to the [minimum] number.
+ * @param minimum the minimum size of the number.
+ * @author Fruxz
+ * @since 1.0
+ */
 fun Byte.minTo(minimum: Byte) = let { if (it >= minimum) it else minimum }
 
+/**
+ * This function limits [this] number to the [minimum] number.
+ * @param minimum the minimum size of the number.
+ * @author Fruxz
+ * @since 1.0
+ */
 fun Short.minTo(minimum: Short) = let { if (it >= minimum) it else minimum }
 
+/**
+ * This function limits [this] number to the [minimum] number.
+ * @param minimum the minimum size of the number.
+ * @author Fruxz
+ * @since 1.0
+ */
 fun Int.minTo(minimum: Int) = let { if (it >= minimum) it else minimum }
 
+/**
+ * This function limits [this] number to the [minimum] number.
+ * @param minimum the minimum size of the number.
+ * @author Fruxz
+ * @since 1.0
+ */
 fun Long.minTo(minimum: Long) = let { if (it >= minimum) it else minimum }
 
+/**
+ * This function limits [this] number to the [minimum] number.
+ * @param minimum the minimum size of the number.
+ * @author Fruxz
+ * @since 1.0
+ */
 fun Float.minTo(minimum: Float) = let { if (it >= minimum) it else minimum }
 
+/**
+ * This function limits [this] number to the [minimum] number.
+ * @param minimum the minimum size of the number.
+ * @author Fruxz
+ * @since 1.0
+ */
 fun Double.minTo(minimum: Double) = let { if (it >= minimum) it else minimum }
 
+/**
+ * This function limits [this] number to the [minimum] number.
+ * @param minimum the minimum size of the number.
+ * @author Fruxz
+ * @since 1.0
+ */
 fun UByte.minTo(minimum: UByte) = let { if (it >= minimum) it else minimum }
 
+/**
+ * This function limits [this] number to the [minimum] number.
+ * @param minimum the minimum size of the number.
+ * @author Fruxz
+ * @since 1.0
+ */
 fun UShort.minTo(minimum: UShort) = let { if (it >= minimum) it else minimum }
 
+/**
+ * This function limits [this] number to the [minimum] number.
+ * @param minimum the minimum size of the number.
+ * @author Fruxz
+ * @since 1.0
+ */
 fun UInt.minTo(minimum: UInt) = let { if (it >= minimum) it else minimum }
 
+/**
+ * This function limits [this] number to the [minimum] number.
+ * @param minimum the minimum size of the number.
+ * @author Fruxz
+ * @since 1.0
+ */
 fun ULong.minTo(minimum: ULong) = let { if (it >= minimum) it else minimum }
 
 // limit range functions
 
+/**
+ * This function limits [this] number to the [range] number range.
+ * @param range the range, where the number will be limited.
+ * @author Fruxz
+ * @since 1.0
+ */
 fun Byte.limitTo(range: IntRange) = let { if (it in range) it else range.first }
 
+/**
+ * This function limits [this] number to the [range] number range.
+ * @param range the range, where the number will be limited.
+ * @author Fruxz
+ * @since 1.0
+ */
 fun Short.limitTo(range: IntRange) = let { if (it in range) it else range.first }
 
+/**
+ * This function limits [this] number to the [range] number range.
+ * @param range the range, where the number will be limited.
+ * @author Fruxz
+ * @since 1.0
+ */
 fun Int.limitTo(range: IntRange) = let { if (it in range) it else range.first }
 
+/**
+ * This function limits [this] number to the [range] number range.
+ * @param range the range, where the number will be limited.
+ * @author Fruxz
+ * @since 1.0
+ */
 fun Long.limitTo(range: LongRange) = let { if (it in range) it else range.first }
 
+/**
+ * This function limits [this] number to the [range] number range.
+ * @param range the range, where the number will be limited.
+ * @author Fruxz
+ * @since 1.0
+ */
 fun Float.limitTo(range: ClosedFloatingPointRange<Float>) = let { if (it in range) it else range.start }
 
+/**
+ * This function limits [this] number to the [range] number range.
+ * @param range the range, where the number will be limited.
+ * @author Fruxz
+ * @since 1.0
+ */
 fun Double.limitTo(range: ClosedFloatingPointRange<Double>) = let { if (it in range) it else range.start }
 
+/**
+ * This function limits [this] number to the [range] number range.
+ * @param range the range, where the number will be limited.
+ * @author Fruxz
+ * @since 1.0
+ */
 fun UByte.limitTo(range: UIntRange) = let { if (it in range) it else range.first }
 
+/**
+ * This function limits [this] number to the [range] number range.
+ * @param range the range, where the number will be limited.
+ * @author Fruxz
+ * @since 1.0
+ */
 fun UShort.limitTo(range: UIntRange) = let { if (it in range) it else range.first }
 
+/**
+ * This function limits [this] number to the [range] number range.
+ * @param range the range, where the number will be limited.
+ * @author Fruxz
+ * @since 1.0
+ */
 fun UInt.limitTo(range: UIntRange) = let { if (it in range) it else range.first }
 
+/**
+ * This function limits [this] number to the [range] number range.
+ * @param range the range, where the number will be limited.
+ * @author Fruxz
+ * @since 1.0
+ */
 fun ULong.limitTo(range: ULongRange) = let { if (it in range) it else range.first }
 
 // limit progression functions
 
+/**
+ * This function limits [this] number to the [progression] number range.
+ * @param progression the allowed range, where the number will be limited.
+ * @author Fruxz
+ * @since 1.0
+ */
 fun Byte.limitTo(progression: IntProgression) = let { if (it.toInt() in progression) it else progression.first }
 
+/**
+ * This function limits [this] number to the [progression] number range.
+ * @param progression the allowed range, where the number will be limited.
+ * @author Fruxz
+ * @since 1.0
+ */
 fun Short.limitTo(progression: IntProgression) = let { if (it.toInt() in progression) it else progression.first }
 
+/**
+ * This function limits [this] number to the [progression] number range.
+ * @param progression the allowed range, where the number will be limited.
+ * @author Fruxz
+ * @since 1.0
+ */
 fun Int.limitTo(progression: IntProgression) = let { if (it in progression) it else progression.first }
 
+/**
+ * This function limits [this] number to the [progression] number range.
+ * @param progression the allowed range, where the number will be limited.
+ * @author Fruxz
+ * @since 1.0
+ */
 fun Long.limitTo(progression: LongProgression) = let { if (it in progression) it else progression.first }
 
+/**
+ * This function limits [this] number to the [progression] number range.
+ * @param progression the allowed range, where the number will be limited.
+ * @author Fruxz
+ * @since 1.0
+ */
 fun UByte.limitTo(progression: UIntProgression) = let { if (it.toUInt() in progression) it else progression.first }
 
+/**
+ * This function limits [this] number to the [progression] number range.
+ * @param progression the allowed range, where the number will be limited.
+ * @author Fruxz
+ * @since 1.0
+ */
 fun UShort.limitTo(progression: UIntProgression) = let { if (it.toUInt() in progression) it else progression.first }
 
+/**
+ * This function limits [this] number to the [progression] number range.
+ * @param progression the allowed range, where the number will be limited.
+ * @author Fruxz
+ * @since 1.0
+ */
 fun UInt.limitTo(progression: UIntProgression) = let { if (it in progression) it else progression.first }
 
+/**
+ * This function limits [this] number to the [progression] number range.
+ * @param progression the allowed range, where the number will be limited.
+ * @author Fruxz
+ * @since 1.0
+ */
 fun ULong.limitTo(progression: ULongProgression) = let { if (it in progression) it else progression.first }
 
 /**
@@ -193,7 +421,7 @@ fun ULong.limitTo(progression: ULongProgression) = let { if (it in progression) 
  * @since 1.0
  */
 val Double.decimalAsPercent: Percentage
-    get() = Percentage(this)
+	get() = Percentage(this)
 
 /**
  * Creates a [Percentage] object with the [this] [Double]
@@ -202,4 +430,4 @@ val Double.decimalAsPercent: Percentage
  * @since 1.0
  */
 val Float.decimalAsPercent: Percentage
-    get() = Percentage(this.toDouble())
+	get() = Percentage(this.toDouble())
