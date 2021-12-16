72115459a92298042e2f2102d0140f74eef970b0
TheFruxz <cedricspitzer@outlook.de>
2021-12-16 20:54:14 +0100

    Chore(docs): add TerminalConsoleExtension.kt documentation

--- JET-JVM/src/main/kotlin/de/jet/jvm/application/extension/TerminalConsoleExtension.kt
+++ JET-JVM/src/main/kotlin/de/jet/jvm/application/extension/TerminalConsoleExtension.kt
@@ -4,6 +4,11 @@ import de.jet.jvm.application.console.interchange.ConsoleInterchange
 import de.jet.jvm.extension.input.requestTerminalInterchangeInput
 import kotlin.reflect.KFunction1
 
+/**
+ * Extension to easily create terminal input requests inside a application
+ * @author Fruxz
+ * @since 1.0
+ */
 object TerminalConsoleExtension : AppExtension<MutableList<ConsoleInterchange>, Unit, Unit> {
 
 	override val identity = "TerminalConsole"
