ffdaacfa7d6530160834907ac916e548e8c5d098
TheFruxz <cedricspitzer@outlook.de>
2021-12-16 13:02:17 +0100

    Feature: add nextOrFirst() and previousOrLast() enum extension functions

--- JET-JVM/src/main/kotlin/de/jet/jvm/extension/classes/Enum.kt
+++ JET-JVM/src/main/kotlin/de/jet/jvm/extension/classes/Enum.kt
@@ -15,3 +15,17 @@ inline fun <reified T : Enum<T>> Enum<T>.next() = enumValues<T>()[(ordinal + 1).
  * @since 1.0
  */
 inline fun <reified T : Enum<T>> Enum<T>.previous() = enumValues<T>()[(ordinal - 1).limitTo(0..enumValues<T>().lastIndex)]
+
+/**
+ * Returns the next enum in the ordinal order, or if this enum is already the last enum, returns the first enum
+ * @author Fruxz
+ * @since 1.0
+ */
+inline fun <reified T :Enum<T>> Enum<T>.nextOrFirst() = if (ordinal == enumValues<T>().lastIndex) enumValues<T>().first() else next()
+
+/**
+ * Returns the previous enum in the ordinal order, or if this enum is already the first enum, returns the last enum
+ * @author Fruxz
+ * @since 1.0
+ */
+inline fun <reified T :Enum<T>> Enum<T>.previousOrLast() = if (ordinal == enumValues<T>().lastIndex) enumValues<T>().last() else previous()
