1715e01146f0e2c79ad47f027637bf53ca5fb2da
TheFruxz <cedricspitzer@outlook.de>
2021-12-16 16:15:24 +0100

    Feature: add the ability, to modify the json module

--- JET-JVM/src/main/kotlin/de/jet/jvm/extension/data/JSON.kt
+++ JET-JVM/src/main/kotlin/de/jet/jvm/extension/data/JSON.kt
@@ -1,18 +1,67 @@
 package de.jet.jvm.extension.data
 
+import de.jet.jvm.extension.objects.trustOrThrow
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.modules.SerializersModule
+import kotlinx.serialization.modules.SerializersModuleBuilder
 
-val jsonBase = Json {
-	prettyPrint = true
-	isLenient = true
-	ignoreUnknownKeys = true
-	coerceInputValues = true
-	encodeDefaults = true
-	explicitNulls = true
-	allowStructuredMapKeys = true
-	allowSpecialFloatingPointValues = true
+internal val runningModuleModifications = mutableListOf<SerializersModuleBuilder.() -> Unit>()
+internal var lastKnownModifications = listOf<SerializersModuleBuilder.() -> Unit>()
+
+/**
+ * This value returns the current [Json] from the cached value,
+ * or creates a new one, if no Json exists, or its modifications
+ * are outdated.
+ * @sample toJson
+ * @author Fruxz
+ * @since 1.0
+ */
+@Suppress("JSON_FORMAT_REDUNDANT")
+val jsonBase: Json
+	get() {
+		if (jsonBaseState == null || lastKnownModifications != runningModuleModifications) {
+			Json {
+				prettyPrint = true
+				isLenient = true
+				ignoreUnknownKeys = true
+				coerceInputValues = true
+				encodeDefaults = true
+				explicitNulls = true
+				allowStructuredMapKeys = true
+				allowSpecialFloatingPointValues = true
+				serializersModule = SerializersModule {
+					runningModuleModifications.forEach {
+						this.apply(it)
+					}
+				}
+			}.let { constructed ->
+				jsonBaseState = constructed
+				return constructed
+			}
+		} else
+			return jsonBaseState.trustOrThrow(IllegalStateException("Json state cannot be null"))
+	}
+
+/**
+ * The current cached state of the json base,
+ * can change, if modification-list updates!
+ * @see jsonBase
+ * @author Fruxz
+ * @since 1.0
+ */
+private var jsonBaseState: Json? = null
+
+/**
+ * Adds a custom modification to the [SerializersModule] during its build process
+ * for the [Json] object, used at the [toJson] and [fromJson] functions.
+ * @param process the modification to the [SerializersModuleBuilder] in the building process
+ * @author Fruxz
+ * @since 1.0
+ */
+fun addJetJsonModification(process: SerializersModuleBuilder.() -> Unit) {
+	runningModuleModifications += process
 }
 
 /**
