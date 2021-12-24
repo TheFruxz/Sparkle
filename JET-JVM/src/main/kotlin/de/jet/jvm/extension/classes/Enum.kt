59e4a373a6791aeaa60fe63007a1bcce24024fac
TheFruxz <cedricspitzer@outlook.de>
2021-12-24 12:49:39 +0100

    Feature: add & use nextOrNull() and previousOrNull() functions

--- JET-JVM/src/main/kotlin/de/jet/jvm/extension/classes/Enum.kt
+++ JET-JVM/src/main/kotlin/de/jet/jvm/extension/classes/Enum.kt
@@ -17,15 +17,29 @@ inline fun <reified T : Enum<T>> Enum<T>.next() = enumValues<T>()[(ordinal + 1).
 inline fun <reified T : Enum<T>> Enum<T>.previous() = enumValues<T>()[(ordinal - 1).limitTo(0..enumValues<T>().lastIndex)]
 
 /**
+ * Returns the next enum in the ordinal order, or null, if this is already the last enum.
+ * @author Fruxz
+ * @since 1.0
+ */
+inline fun <reified T : Enum<T>> Enum<T>.nextOrNull() = if (ordinal == enumValues<T>().lastIndex) null else next()
+
+/**
+ * Returns the previous enum in the ordinal order, or null, if this is already the first enum.
+ * @author Fruxz
+ * @since 1.0
+ */
+inline fun <reified T : Enum<T>> Enum<T>.previousOrNull() = if (ordinal == 0) null else previous()
+
+/**
  * Returns the next enum in the ordinal order, or if this enum is already the last enum, returns the first enum
  * @author Fruxz
  * @since 1.0
  */
-inline fun <reified T : Enum<T>> Enum<T>.nextOrFirst() = if (ordinal == enumValues<T>().lastIndex) enumValues<T>().first() else next()
+inline fun <reified T : Enum<T>> Enum<T>.nextOrFirst() = nextOrNull() ?: enumValues<T>().first()
 
 /**
  * Returns the previous enum in the ordinal order, or if this enum is already the first enum, returns the last enum
  * @author Fruxz
  * @since 1.0
  */
-inline fun <reified T : Enum<T>> Enum<T>.previousOrLast() = if (ordinal == 0) enumValues<T>().last() else previous()
+inline fun <reified T : Enum<T>> Enum<T>.previousOrLast() = previousOrNull() ?: enumValues<T>().last()
