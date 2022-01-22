bdb77565287c6aaf48e83cca4581fb1d3620fa84
TheFruxz <cedricspitzer@outlook.de>
2022-01-22 15:05:16 +0100

    Feature: add more Logger functions

--- JET-JVM/src/main/kotlin/de/jet/jvm/extension/logging/Logger.kt
+++ JET-JVM/src/main/kotlin/de/jet/jvm/extension/logging/Logger.kt
@@ -3,6 +3,11 @@ package de.jet.jvm.extension.logging
 import kotlin.reflect.KClass
 import java.util.logging.Logger as JavaUtilLogger
 
-fun getLogger(kclass: KClass<*>): JavaUtilLogger = JavaUtilLogger.getLogger(kclass.simpleName)
+fun getLogger(name: String): JavaUtilLogger = JavaUtilLogger.getLogger(name)
+
+fun getLogger(name: String, resourceBundleName: String): JavaUtilLogger = JavaUtilLogger.getLogger(name, resourceBundleName)
+
+fun getLogger(kclass: KClass<*>) =
+	getLogger(kclass.simpleName ?: "generic")
 
 fun <T : Any> T.getItsLogger() = getLogger(this::class)
