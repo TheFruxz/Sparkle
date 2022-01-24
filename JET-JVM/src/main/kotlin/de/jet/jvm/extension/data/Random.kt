4e462965a5a61b8245d7be0eb846e332d3e23bf1
TheFruxz <cedricspitzer@outlook.de>
2022-01-24 16:47:02 +0100

    Feature: more randomizer options

--- JET-JVM/src/main/kotlin/de/jet/jvm/extension/data/Random.kt
+++ JET-JVM/src/main/kotlin/de/jet/jvm/extension/data/Random.kt
@@ -2,6 +2,9 @@ package de.jet.jvm.extension.data
 
 import de.jet.jvm.extension.collection.mapToString
 import de.jet.jvm.extension.collection.stackRandom
+import de.jet.jvm.extension.data.RandomTagType.MIXED_CASE
+import de.jet.jvm.extension.data.RandomTagType.ONLY_UPPERCASE
+import de.jet.jvm.extension.switchResult
 import kotlin.random.Random
 import kotlin.random.nextInt
 
@@ -28,12 +31,27 @@ fun randomInt(range: IntRange) = Random.nextInt(range)
  * random letters & numbers.
  * The template of the Tag is '#[letters & numbers]'.
  * @param size the amount of mixed letters & numbers, which the Tag should have ([size]+1 == tag.length)
+ * @param hashtag if true, the Tag will start with a '#'
+ * @param tagType the type of the Tag creation (if uppercase, lowercase or mixed-case)
  * @return a random Tag with the # at the start of the generated Tag
  * @author Fruxz
  * @since 1.0
  */
-fun randomTag(size: Int = 5): String {
-	return "#" + (listOf("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z") + (0..9))
+fun randomTag(size: Int = 5, hashtag: Boolean = true, tagType: RandomTagType = ONLY_UPPERCASE): String {
+	val letters = listOf("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
+		.map { when (tagType) {
+			ONLY_UPPERCASE -> it.uppercase()
+			MIXED_CASE -> (0..1).random().let { r -> if (r == 0) it.uppercase() else it }
+			else -> it
+		} }
+
+	return hashtag.switchResult("#", "") + (letters + (0..9))
 		.mapToString()
 		.stackRandom(size)
+}
+
+enum class RandomTagType {
+	ONLY_UPPERCASE,
+	ONLY_LOWERCASE,
+	MIXED_CASE;
 }
