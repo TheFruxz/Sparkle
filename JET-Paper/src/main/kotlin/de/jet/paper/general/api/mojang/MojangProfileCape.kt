5e779f9e13ac4aedc376a20749ea79a0e1aaccdd
TheFruxz <cedricspitzer@outlook.de>
2022-01-31 12:44:39 +0100

    Fix: remove experimental serialization api

--- JET-Paper/src/main/kotlin/de/jet/paper/general/api/mojang/MojangProfileCape.kt
+++ JET-Paper/src/main/kotlin/de/jet/paper/general/api/mojang/MojangProfileCape.kt
@@ -1,11 +1,9 @@
 package de.jet.paper.general.api.mojang
 
-import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.JsonNames
 
-@ExperimentalSerializationApi
 @Serializable
 @SerialName("MojangProfileCape")
 data class MojangProfileCape(
