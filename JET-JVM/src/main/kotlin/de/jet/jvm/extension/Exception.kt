adda2e75d7ddf3d8b1d6dddff62c8bc77511a6d8
TheFruxz <cedricspitzer@outlook.de>
2022-02-02 18:34:48 +0100

    Fix: remove unit-duplicates

--- JET-JVM/src/main/kotlin/de/jet/jvm/extension/Exception.kt
+++ JET-JVM/src/main/kotlin/de/jet/jvm/extension/Exception.kt
@@ -35,21 +35,6 @@ fun <O> doTry(tryBlock: () -> O) =
 /**
  * Try to execute the code specified inside the [process] function.
  * If an exception is thrown, the [catchException] function will be executed.
- * @param process the code to execute
- * @author Fruxz
- * @since 1.0
- */
-fun tryToCatch(process: () -> Unit) {
-	try {
-		process()
-	} catch (e: Exception) {
-		catchException(e)
-	}
-}
-
-/**
- * Try to execute the code specified inside the [process] function.
- * If an exception is thrown, the [catchException] function will be executed.
  * @param A (short for air) is the type of the surrounding block or the object, where it is called from
  * @param process the code to execute
  * @author Fruxz
@@ -73,22 +58,6 @@ fun <A> A.tryToCatch(process: A.() -> Unit) {
  * @since 1.0
  */
 fun <A, T> A.tryToResult(process: A.() -> T): Result<T> {
-	return try {
-		Result.success(process())
-	} catch (e: Exception) {
-		Result.failure(e)
-	}
-}
-
-/**
- * Try return the value and returns the result inside a [Result] object.
- * @param T is the return type of the process
- * @param process the process to execute, returning the normal value as [T]
- * @return the value returned by the [process] as a [Result]
- * @author Fruxz
- * @since 1.0
- */
-fun <T> tryToResult(process: () -> T): Result<T> {
 	return try {
 		Result.success(process())
 	} catch (e: Exception) {
@@ -123,18 +92,5 @@ fun <A, R, T : R> A.tryOrElse(other: T, process: A.() -> R): R {
  * @since 1.0
  */
 fun <A, T> A.tryOrNull(process: A.() -> T): T? {
-	return tryToResult(process).getOrNull()
-}
-
-/**
- * Try return the value returning of the [process] or returns null.
- * null-return is triggered by a thrown [Exception]
- * @param T is the return type of the process
- * @param process the process to execute, returning the normal value as [T]
- * @return the value returned by the [process] or null
- * @author Fruxz
- * @since 1.0
- */
-fun <T> tryOrNull(process: () -> T): T? {
 	return tryToResult(process).getOrNull()
 }
