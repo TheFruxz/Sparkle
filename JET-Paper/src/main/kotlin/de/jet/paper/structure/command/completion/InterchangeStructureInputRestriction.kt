8c3c564c6c41f61ae30e9eca892e3b89b48c3cf4
TheFruxz <cedricspitzer@outlook.de>
2022-02-11 23:38:49 +0100

    Feature: add transformer to InterchangeStructureInputRestricition
    Restritions now have a target type and a transformer to this type

--- JET-Paper/src/main/kotlin/de/jet/paper/structure/command/completion/InterchangeStructureInputRestriction.kt
+++ JET-Paper/src/main/kotlin/de/jet/paper/structure/command/completion/InterchangeStructureInputRestriction.kt
@@ -1,36 +1,45 @@
 package de.jet.paper.structure.command.completion
 
+import de.jet.jvm.extension.empty
 import de.jet.jvm.extension.math.isDouble
 import de.jet.jvm.extension.math.isLong
 
-interface InterchangeStructureInputRestriction {
+interface InterchangeStructureInputRestriction<DATATYPE> {
 
 	fun isValid(input: String): Boolean
 
+	fun transformer(input: String): DATATYPE
+
 	companion object {
 
-		val NONE = object : InterchangeStructureInputRestriction {
+		val NONE = object : InterchangeStructureInputRestriction<Unit> {
 			override fun isValid(input: String) = false
+			override fun transformer(input: String) = empty()
 		}
 
-		val ANY = object : InterchangeStructureInputRestriction {
+		val ANY = object : InterchangeStructureInputRestriction<String> {
 			override fun isValid(input: String) = true
+			override fun transformer(input: String) = input
 		}
 
-		val STRING = object : InterchangeStructureInputRestriction {
+		val STRING = object : InterchangeStructureInputRestriction<String> {
 			override fun isValid(input: String) = true
+			override fun transformer(input: String) = input
 		}
 
-		val LONG = object : InterchangeStructureInputRestriction {
+		val LONG = object : InterchangeStructureInputRestriction<Long> {
 			override fun isValid(input: String) = input.isLong()
+			override fun transformer(input: String) = input.toLong()
 		}
 
-		val DOUBLE = object : InterchangeStructureInputRestriction {
+		val DOUBLE = object : InterchangeStructureInputRestriction<Double> {
 			override fun isValid(input: String) = input.isDouble()
+			override fun transformer(input: String) = input.toDouble()
 		}
 
-		val BOOLEAN = object : InterchangeStructureInputRestriction {
+		val BOOLEAN = object : InterchangeStructureInputRestriction<Boolean> {
 			override fun isValid(input: String) = input.toBooleanStrictOrNull() != null
+			override fun transformer(input: String) = input.toBooleanStrict()
 		}
 
 	}
