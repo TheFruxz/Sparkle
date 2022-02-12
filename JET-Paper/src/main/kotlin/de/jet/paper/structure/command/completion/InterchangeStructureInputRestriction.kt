cf15e26f176a64957b9ab18ee5f41e7883d39984
TheFruxz <cedricspitzer@outlook.de>
2022-02-12 14:52:35 +0100

    Feature: add player input restricitions

--- JET-Paper/src/main/kotlin/de/jet/paper/structure/command/completion/InterchangeStructureInputRestriction.kt
+++ JET-Paper/src/main/kotlin/de/jet/paper/structure/command/completion/InterchangeStructureInputRestriction.kt
@@ -3,6 +3,12 @@ package de.jet.paper.structure.command.completion
 import de.jet.jvm.extension.empty
 import de.jet.jvm.extension.math.isDouble
 import de.jet.jvm.extension.math.isLong
+import de.jet.jvm.extension.tryOrNull
+import de.jet.paper.extension.paper.getOfflinePlayer
+import de.jet.paper.extension.paper.getPlayer
+import org.bukkit.OfflinePlayer
+import org.bukkit.entity.Player
+import java.util.*
 
 interface InterchangeStructureInputRestriction<DATATYPE> {
 
@@ -40,6 +46,16 @@ interface InterchangeStructureInputRestriction<DATATYPE> {
 		val BOOLEAN = object : InterchangeStructureInputRestriction<Boolean> {
 			override fun isValid(input: String) = input.toBooleanStrictOrNull() != null
 			override fun transformer(input: String) = input.toBooleanStrict()
+		}
+
+		val ONLINE_PLAYER = object : InterchangeStructureInputRestriction<Player> {
+			override fun isValid(input: String) = (getPlayer(input) ?: tryOrNull { getPlayer(UUID.fromString(input)) }) != null
+			override fun transformer(input: String) = getPlayer(input) ?: getPlayer(UUID.fromString(input))!!
+		}
+
+		val OFFLINE_PLAYER = object : InterchangeStructureInputRestriction<OfflinePlayer> {
+			override fun isValid(input: String) = (tryOrNull { getOfflinePlayer(UUID.fromString(input)) } ?: getOfflinePlayer(input)).name != null
+			override fun transformer(input: String) = tryOrNull { getOfflinePlayer(UUID.fromString(input)) } ?: getOfflinePlayer(input)
 		}
 
 	}
