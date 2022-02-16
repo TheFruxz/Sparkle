92444fe742ea8e93d5d4c954d27eed3f559d3bbe
TheFruxz <cedricspitzer@outlook.de>
2022-02-16 13:34:19 +0100

    Feature: Add base structure of WorldManager component

---
+++ JET-Paper/src/main/kotlin/de/jet/paper/app/component/world/WorldManagerComponent.kt
@@ -0,0 +1,17 @@
+package de.jet.paper.app.component.world
+
+import de.jet.paper.extension.system
+import de.jet.paper.structure.component.Component
+import de.jet.paper.structure.component.SmartComponent
+
+class WorldManagerComponent : SmartComponent(system, RunType.AUTOSTART_MUTABLE) {
+
+    override val thisIdentity = "WorldManager"
+
+    override fun component() {
+
+
+
+    }
+
+}
---
+++ JET-Paper/src/main/kotlin/de/jet/paper/app/component/world/WorldMangagerInterchange.kt
@@ -0,0 +1,6 @@
+package de.jet.paper.app.component.world
+
+import de.jet.paper.structure.command.BranchedInterchange
+
+class WorldManagerInterchange : BranchedInterchange() {
+}
