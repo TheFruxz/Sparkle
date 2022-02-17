2c00e1fc53b6f4f36fb4a19cf86b9879e5819f69
TheFruxz <cedricspitzer@outlook.de>
2022-02-17 13:15:42 +0100

    Fix: working world placeholder

--- JET-Paper/src/main/kotlin/de/jet/paper/app/component/world/WorldMangagerInterchange.kt
+++ JET-Paper/src/main/kotlin/de/jet/paper/app/component/world/WorldMangagerInterchange.kt
@@ -1,6 +1,8 @@
 package de.jet.paper.app.component.world
 
 import de.jet.paper.structure.command.BranchedInterchange
+import de.jet.paper.structure.command.completion.buildInterchangeStructure
 
-class WorldManagerInterchange : BranchedInterchange() {
-}
+class WorldManagerInterchange : BranchedInterchange("world", buildInterchangeStructure {
+
+})
