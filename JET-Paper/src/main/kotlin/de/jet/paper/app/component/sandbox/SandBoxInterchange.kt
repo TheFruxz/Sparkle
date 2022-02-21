465faed282676c4d7aa716df306d478738fa0375
TheFruxz <cedricspitzer@outlook.de>
2022-02-21 15:15:29 +0100

    Fix: sandbox interchange has no permission protection

--- JET-Paper/src/main/kotlin/de/jet/paper/app/component/sandbox/SandBoxInterchange.kt
+++ JET-Paper/src/main/kotlin/de/jet/paper/app/component/sandbox/SandBoxInterchange.kt
@@ -26,196 +26,205 @@ import de.jet.paper.structure.command.completion.ignoreCase
 import de.jet.paper.structure.command.completion.isNotRequired
 import de.jet.paper.tool.display.message.Transmission.Level.*
 
-internal class SandBoxInterchange : StructuredInterchange("sandbox", buildInterchangeStructure {
+internal class SandBoxInterchange : StructuredInterchange(
+	"sandbox",
+	protectedAccess = true,
+	structure = buildInterchangeStructure {
 
-	branch {
-		addContent(CompletionComponent.static("dropAll"))
+		branch {
+			addContent(CompletionComponent.static("dropAll"))
 
-		ignoreCase()
+			ignoreCase()
 
-		concludedExecution {
+			concludedExecution {
 
-			if (allSandBoxes.isNotEmpty()) {
+				if (allSandBoxes.isNotEmpty()) {
 
-				val message = lang("interchange.internal.sandbox.dropAll")
-					.replaceVariables("amount" to allSandBoxes.size)
+					val message = lang("interchange.internal.sandbox.dropAll")
+						.replaceVariables("amount" to allSandBoxes.size)
 
-				destroyAllSandBoxes()
+					destroyAllSandBoxes()
 
-				message.notification(APPLIED, executor).display()
+					message.notification(APPLIED, executor).display()
 
-			} else
-				lang("interchange.internal.sandbox.noFound")
-					.notification(FAIL, executor).display()
+				} else
+					lang("interchange.internal.sandbox.noFound")
+						.notification(FAIL, executor).display()
+			}
+
 		}
 
-	}
+		branch {
+			addContent(CompletionComponent.static("runAll"))
 
-	branch {
-		addContent(CompletionComponent.static("runAll"))
+			ignoreCase()
 
-		ignoreCase()
+			concludedExecution {
 
-		concludedExecution {
+				if (allSandBoxes.isNotEmpty()) {
+					allSandBoxes.forEach { sandBox ->
 
-			if (allSandBoxes.isNotEmpty()) {
-				allSandBoxes.forEach { sandBox ->
-
-					sandBox.execute(executor)
-
-				}
-
-				lang("interchange.internal.sandbox.runAll")
-					.replaceVariables("amount" to allSandBoxes.size)
-					.notification(APPLIED, executor).display()
-
-			} else
-				lang("interchange.internal.sandbox.noFound")
-					.notification(FAIL, executor).display()
-		}
-
-	}
-
-	branch {
-		addContent(CompletionComponent.static("list"))
-
-		ignoreCase()
-
-		fun displaySandBoxes(executor: InterchangeExecutor, page: Int) {
-			val pageValue = allSandBoxes.page(page, 6)
-
-			if (pageValue.content.isNotEmpty()) {
-				buildString {
-					appendLine(
-						lang("interchange.internal.sandbox.list.header").replaceVariables(
-							"p1" to pageValue.page+1,
-							"p2" to pageValue.pages,
-						)
-					)
-
-					pageValue.content.withIndex().forEach {
-
-						lang("interchange.internal.sandbox.list.line").replaceVariables(
-							"sandbox" to it.value.identity,
-							"vendor" to it.value.vendor.appLabel,
-						).let { message ->
-							if (it.index == allSandBoxes.indices.last) {
-								append(message)
-							} else
-								appendLine(message)
-						}
+						sandBox.execute(executor)
 
 					}
 
-				}.notification(INFO, executor).display()
-			} else
-				lang("interchange.internal.sandbox.noFound")
-					.notification(FAIL, executor).display()
-		}
-
-		concludedExecution {
-
-			displaySandBoxes(executor, 0)
-
-			SUCCESS
-		}
-
-		branch {
-			addContent(CompletionComponent.asset(CompletionAsset(
-				vendor = system,
-				thisIdentity = "Page",
-				true,
-				listOf(InterchangeStructureInputRestriction.LONG),
-				generator = {
-					(1..ceilToInt(allSandBoxes.size.toDouble() / 6)).mapToString()
-				},
-			)))
-
-			isNotRequired()
-
-			execution {
-				val page = getInput(1, InterchangeStructureInputRestriction.LONG).toInt()-1
-
-				if (page >= 0) {
-					displaySandBoxes(executor, page)
-				} else
-					return@execution WRONG_USAGE
-
-				return@execution SUCCESS
-			}
-
-		}
-
-	}
-
-	branch {
-
-		addContent(CompletionComponent.static("do"))
-
-		ignoreCase()
-
-		branch {
-
-			addContent(Companion.asset(CompletionAsset.SANDBOX))
-
-			branch {
-
-				addContent(Companion.static("drop"))
-
-				concludedExecution {
-					val sandBox = getSandBox(getInput(1))!!
-
-					destroySandBox(sandBox)
-
-					lang("interchange.internal.sandbox.drop")
-						.replaceVariables("sandbox" to sandBox.identity)
+					lang("interchange.internal.sandbox.runAll")
+						.replaceVariables("amount" to allSandBoxes.size)
 						.notification(APPLIED, executor).display()
 
-				}
-
+				} else
+					lang("interchange.internal.sandbox.noFound")
+						.notification(FAIL, executor).display()
 			}
 
-			branch {
+		}
 
-				addContent(Companion.static("run"))
+		branch {
+			addContent(CompletionComponent.static("list"))
 
-				concludedExecution {
-					val sandBox = getSandBox(getInput(1))!!
+			ignoreCase()
 
-					sandBox.execute(executor)
+			fun displaySandBoxes(executor: InterchangeExecutor, page: Int) {
+				val pageValue = allSandBoxes.page(page, 6)
 
-				}
-
-			}
-
-			branch {
-
-				addContent(Companion.static("info"))
-
-				concludedExecution {
-					val sandBox = getSandBox(getInput(1))!!
-
+				if (pageValue.content.isNotEmpty()) {
 					buildString {
-						appendLine(lang("interchange.internal.sandbox.info.header"))
-
-						mapOf(
-							"VendorId" to sandBox.vendor.identity,
-							"VendorLabel" to sandBox.vendor.appLabel,
-							"Identity" to sandBox.identity,
-							"Since" to sandBox.creationTime.durationTo(Calendar.now()).toString(),
-							"CreationPos" to sandBox.creationLocation,
-							"Cached Calls" to (JetCache.registeredSandBoxCalls[sandBox.identityObject]
-								?: "none")
-						).forEach { (key, value) ->
-							appendLine(
-								lang("interchange.internal.sandbox.info.line").replaceVariables(
-									"key" to key,
-									"value" to value,
-								)
+						appendLine(
+							lang("interchange.internal.sandbox.list.header").replaceVariables(
+								"p1" to pageValue.page + 1,
+								"p2" to pageValue.pages,
 							)
+						)
+
+						pageValue.content.withIndex().forEach {
+
+							lang("interchange.internal.sandbox.list.line").replaceVariables(
+								"sandbox" to it.value.identity,
+								"vendor" to it.value.vendor.appLabel,
+							).let { message ->
+								if (it.index == allSandBoxes.indices.last) {
+									append(message)
+								} else
+									appendLine(message)
+							}
+
 						}
 
 					}.notification(INFO, executor).display()
+				} else
+					lang("interchange.internal.sandbox.noFound")
+						.notification(FAIL, executor).display()
+			}
+
+			concludedExecution {
+
+				displaySandBoxes(executor, 0)
+
+				SUCCESS
+			}
+
+			branch {
+				addContent(
+					CompletionComponent.asset(
+						CompletionAsset(
+							vendor = system,
+							thisIdentity = "Page",
+							true,
+							listOf(InterchangeStructureInputRestriction.LONG),
+							generator = {
+								(1..ceilToInt(allSandBoxes.size.toDouble() / 6)).mapToString()
+							},
+						)
+					)
+				)
+
+				isNotRequired()
+
+				execution {
+					val page = getInput(1, InterchangeStructureInputRestriction.LONG).toInt() - 1
+
+					if (page >= 0) {
+						displaySandBoxes(executor, page)
+					} else
+						return@execution WRONG_USAGE
+
+					return@execution SUCCESS
+				}
+
+			}
+
+		}
+
+		branch {
+
+			addContent(CompletionComponent.static("do"))
+
+			ignoreCase()
+
+			branch {
+
+				addContent(Companion.asset(CompletionAsset.SANDBOX))
+
+				branch {
+
+					addContent(Companion.static("drop"))
+
+					concludedExecution {
+						val sandBox = getSandBox(getInput(1))!!
+
+						destroySandBox(sandBox)
+
+						lang("interchange.internal.sandbox.drop")
+							.replaceVariables("sandbox" to sandBox.identity)
+							.notification(APPLIED, executor).display()
+
+					}
+
+				}
+
+				branch {
+
+					addContent(Companion.static("run"))
+
+					concludedExecution {
+						val sandBox = getSandBox(getInput(1))!!
+
+						sandBox.execute(executor)
+
+					}
+
+				}
+
+				branch {
+
+					addContent(Companion.static("info"))
+
+					concludedExecution {
+						val sandBox = getSandBox(getInput(1))!!
+
+						buildString {
+							appendLine(lang("interchange.internal.sandbox.info.header"))
+
+							mapOf(
+								"VendorId" to sandBox.vendor.identity,
+								"VendorLabel" to sandBox.vendor.appLabel,
+								"Identity" to sandBox.identity,
+								"Since" to sandBox.creationTime.durationTo(Calendar.now()).toString(),
+								"CreationPos" to sandBox.creationLocation,
+								"Cached Calls" to (JetCache.registeredSandBoxCalls[sandBox.identityObject]
+									?: "none")
+							).forEach { (key, value) ->
+								appendLine(
+									lang("interchange.internal.sandbox.info.line").replaceVariables(
+										"key" to key,
+										"value" to value,
+									)
+								)
+							}
+
+						}.notification(INFO, executor).display()
+
+					}
 
 				}
 
@@ -224,5 +233,4 @@ internal class SandBoxInterchange : StructuredInterchange("sandbox", buildInterc
 		}
 
 	}
-
-})
+)
