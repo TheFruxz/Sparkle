0124275908a9b7a0f362802f77cc04d15a0489df
TheFruxz <cedricspitzer@outlook.de>
2022-02-26 13:27:12 +0100

    Feature: accessible mutable container decision-parameter

--- JET-JVM/src/main/kotlin/de/jet/jvm/extension/container/MutableCollection.kt
+++ JET-JVM/src/main/kotlin/de/jet/jvm/extension/container/MutableCollection.kt
@@ -18,38 +18,32 @@ fun <T> MutableCollection<T>.mutableReplaceWith(collection: Collection<T>) {
 /**
  * If contained, remove; or if not contained add element!
  * @param o The element to be added or removed
+ * @param addToContainer If true, add element to container; else remove element from container
  * @author Fruxz
  * @since 1.0
  */
-fun <T> MutableCollection<T>.toggle(o: T) {
-	if (contains(o))
-		remove(o)
-	else
-		add(o)
+fun <T> MutableCollection<T>.toggle(o: T, addToContainer: Boolean = !contains(o)) {
+	if (addToContainer) add(o) else remove(o)
 }
 
 /**
  * If contained, remove; or if not contained add element!
  * @param o The element to be added or removed
+ * @param addToContainer If true, add element to container; else remove element from container
  * @author Fruxz
  * @since 1.0
  */
-fun <T> MutableSet<T>.toggle(o: T) {
-	if (contains(o))
-		remove(o)
-	else
-		add(o)
+fun <T> MutableSet<T>.toggle(o: T, addToContainer: Boolean = !contains(o)) {
+	if (addToContainer) add(o) else remove(o)
 }
 
 /**
  * If contained, remove; or if not contained add element!
  * @param o The element to be added or removed
+ * @param addToContainer If true, add element to container; else remove element from container
  * @author Fruxz
  * @since 1.0
  */
-fun <T> MutableList<T>.toggle(o: T) {
-	if (contains(o))
-		remove(o)
-	else
-		add(o)
+fun <T> MutableList<T>.toggle(o: T, addToContainer: Boolean = !contains(o)) {
+	if (addToContainer) add(o) else remove(o)
 }
