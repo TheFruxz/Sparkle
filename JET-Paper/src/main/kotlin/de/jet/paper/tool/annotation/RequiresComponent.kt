1a26c0627887c8d0e7434c2c8d2e94def7228667
TheFruxz <cedricspitzer@outlook.de>
2022-03-02 11:05:12 +0100

    Fix: remove requiresOptIn from RequiresComponent annotation

--- JET-Paper/src/main/kotlin/de/jet/paper/tool/annotation/RequiresComponent.kt
+++ JET-Paper/src/main/kotlin/de/jet/paper/tool/annotation/RequiresComponent.kt
@@ -1,14 +1,13 @@
 package de.jet.paper.tool.annotation
 
 import de.jet.paper.structure.component.Component
-import kotlin.RequiresOptIn.Level.WARNING
 import kotlin.reflect.KClass
 
 @Repeatable
 @MustBeDocumented
-@RequiresOptIn("""
+/*@RequiresOptIn("""
 	This Class/Function/Feature requires a specific on-runtime enabled component running!
 	Opt-In this annotation, if you want to use this feature and actually care about, that
 	the required component is running, to avoid exceptions and other critical issues.
-""", WARNING)
+""", WARNING)*/
 internal annotation class RequiresComponent(val module: KClass<out Component>)
