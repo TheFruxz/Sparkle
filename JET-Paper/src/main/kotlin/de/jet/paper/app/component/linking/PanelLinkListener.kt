d91a3afda0c39372b06446b1f80104e498ad4baa
TheFruxz <cedricspitzer@outlook.de>
2022-03-04 14:04:28 +0100

    Fix: no returned objects at eventhandlers

--- JET-Paper/src/main/kotlin/de/jet/paper/app/component/linking/PanelLinkListener.kt
+++ JET-Paper/src/main/kotlin/de/jet/paper/app/component/linking/PanelLinkListener.kt
@@ -11,12 +11,14 @@ import org.bukkit.event.EventHandler
 internal class PanelLinkListener : EventListener() {
 
 	@EventHandler
-	fun onPanelClick(event: PanelClickEvent) = with(event) { async {
+	fun onPanelClick(event: PanelClickEvent) {
+		with(event) { async {
 			JetCache.panelInteractions.filter { it.key.identity == panel.identity }.forEach { (_, value) ->
 				value[event.clickedSlot]?.forEach { process ->
 					system.coroutineScope.launch { process(this@with) }
 				}
 			}
+		}
 		}
 	}
 
