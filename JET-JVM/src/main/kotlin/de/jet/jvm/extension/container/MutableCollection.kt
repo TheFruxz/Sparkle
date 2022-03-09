8e30aa4a6660ddc71d0f0c57bfb314c72c33231f
TheFruxz <cedricspitzer@outlook.de>
2022-03-09 15:04:14 +0100

    Feature: add more mutable collection feature functions

--- JET-JVM/src/main/kotlin/de/jet/jvm/extension/container/MutableCollection.kt
+++ JET-JVM/src/main/kotlin/de/jet/jvm/extension/container/MutableCollection.kt
@@ -47,3 +47,17 @@ fun <T> MutableSet<T>.toggle(o: T, addToContainer: Boolean = !contains(o)) {
 fun <T> MutableList<T>.toggle(o: T, addToContainer: Boolean = !contains(o)) {
 	if (addToContainer) add(o) else remove(o)
 }
+
+fun <C : MutableCollection<T>, T> C.addIf(element: T, check: (element: T, currentState: C) -> Boolean) {
+	if (check(element, this)) add(element)
+}
+
+fun <C : MutableCollection<T>, T> C.addIfNot(element: T, check: (element: T, currentState: C) -> Boolean) =
+	addIf(element) { it, c -> !check(it, c) }
+
+fun <C : MutableCollection<T>, T> C.addIfContained(element: T) =
+	addIf(element) { _, currentState -> currentState.contains(element) }
+
+fun <C : MutableCollection<T>, T> C.addIfNotContained(element: T) =
+	addIfNot(element) { _, currentState -> currentState.contains(element) }
+
