66b23139aba15a2b4b01003bcabf139239888e3e
TheFruxz <cedricspitzer@outlook.de>
2022-03-10 14:44:45 +0100

    Feature: add 'orEmptyList()', 'orEmptyMutableList()' and 'and' functions
    Add functions to increase usability of collections inside nullability and a new 'and' function, to keep the mutable state during the element-adding process.

--- JET-JVM/src/main/kotlin/de/jet/jvm/extension/container/MutableCollection.kt
+++ JET-JVM/src/main/kotlin/de/jet/jvm/extension/container/MutableCollection.kt
@@ -47,3 +47,44 @@ fun <T> MutableSet<T>.toggle(o: T, addToContainer: Boolean = !contains(o)) {
 fun <T> MutableList<T>.toggle(o: T, addToContainer: Boolean = !contains(o)) {
 	if (addToContainer) add(o) else remove(o)
 }
+
+/**
+ * This function takes a nullable [C] (which is a collection-based object) and
+ * returns an empty list, if it is actually null, or itself, if itself was not
+ * null. This function makes it easy to create new collections, if there is a
+ * nullable collection.
+ * @param C The type of the collection-based object
+ * @param T The type of the elements of the collection-based object
+ * @return An empty list, if [C] was null, or [C] itself, if it was not null
+ * @author Fruxz
+ * @since 1.0
+ */
+fun <C : Collection<T>, T> C?.orEmptyList() = this?.toList() ?: listOf()
+
+/**
+ * This function takes a nullable [C] (which is a mutable-collection-based object) and
+ * returns an empty mutable list, if it is actually null, or itself, if itself was not
+ * null. This function makes it easy to create new mutable collections, if there is a
+ * nullable mutable collection.
+ * @param C The type of the mutable-collection-based object
+ * @param T The type of the elements of the mutable-collection-based object
+ * @return An empty mutable list, if [C] was null, or [C] itself, if it was not null
+ * @author Fruxz
+ * @since 1.0
+ */
+fun <C : MutableCollection<T>, T> C?.orEmptyMutableList() = this?.toMutableList() ?: mutableListOf()
+
+/**
+ * This function allows to get a mutable list of a collection-based object, that
+ * got a new [element] added to it. This function is a little helper besides the
+ * [MutableCollection.plus] function, that only returns an immutable list, instead
+ * of its real type. This function returns a mutable list, that contains the
+ * [element] added to the collection-based object.
+ * @param C The type of the collection-based object
+ * @param T The type of the elements of the collection-based object
+ * @param element The element to be added to the collection-based object
+ * @return A mutable list, that contains the [element] added to the collection-based object
+ * @author Fruxz
+ * @since 1.0
+ */
+infix fun <C : MutableCollection<T>, T> C.and(element: T) = apply { add(element) }
