f4161f915f1c24416f3cf08a747ceb40c9b3bea9
TheFruxz <cedricspitzer@outlook.de>
2022-03-27 23:54:10 +0200

    Chore(docs): update docs pages

---
+++ JET-Paper/src/main/kotlin/de/jet/paper/app/component/app/AppComponent.kt
@@ -0,0 +1,14 @@
+package de.jet.paper.app.component.app
+
+import de.jet.paper.structure.component.Component.RunType.AUTOSTART_IMMUTABLE
+import de.jet.paper.structure.component.SmartComponent
+
+class AppComponent : SmartComponent(AUTOSTART_IMMUTABLE) {
+
+	override val thisIdentity = "AppManagement"
+
+	override suspend fun component() {
+		TODO("Not yet implemented")
+	}
+
+}
---
+++ JET-Paper/src/main/kotlin/de/jet/paper/app/component/app/AppInterchange.kt
@@ -0,0 +1,8 @@
+package de.jet.paper.app.component.app
+
+import de.jet.paper.structure.command.StructuredInterchange
+import de.jet.paper.structure.command.completion.buildInterchangeStructure
+
+class AppInterchange : StructuredInterchange("app", buildInterchangeStructure {
+
+})
