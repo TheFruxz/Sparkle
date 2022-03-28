c11761e28b4a3d3575d8d6a17a7def270d482fe7
TheFruxz <cedricspitzer@outlook.de>
2022-03-28 09:53:15 +0200

    Feature: add easy-to-use getInterchange function

--- JET-Paper/src/main/kotlin/de/jet/paper/extension/interchange/Interchange.kt
+++ JET-Paper/src/main/kotlin/de/jet/paper/extension/interchange/Interchange.kt
@@ -1,5 +1,6 @@
 package de.jet.paper.extension.interchange
 
+import de.jet.paper.app.JetCache
 import org.bukkit.command.CommandSender
 
 @Deprecated("Use InterchangeExecutor instead (JET-Use)", ReplaceWith("de.jet.paper.extension.interchange.InterchangeExecutor"))
@@ -8,3 +9,8 @@ typealias CommandSender = InterchangeExecutor
 typealias InterchangeExecutor = CommandSender
 
 typealias Parameters = Array<out String>
+
+fun getInterchange(label: String, allowPluginIdentity: Boolean = true) =
+    JetCache.registeredInterchanges.firstOrNull {
+        it.label == label || (allowPluginIdentity && it.label == "${it.vendor.name.lowercase()}:$label")
+    }
