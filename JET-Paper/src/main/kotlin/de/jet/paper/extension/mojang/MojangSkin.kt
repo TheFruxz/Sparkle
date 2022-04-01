09d6f9768fe5328f3d84c5a292d176f52f32dcf6
TheFruxz <cedricspitzer@outlook.de>
2022-04-01 16:47:45 +0200

    Feature: add applySkin to SkullMeta functions

--- JET-Paper/src/main/kotlin/de/jet/paper/extension/mojang/MojangSkin.kt
+++ JET-Paper/src/main/kotlin/de/jet/paper/extension/mojang/MojangSkin.kt
@@ -1,10 +1,17 @@
 package de.jet.paper.extension.mojang
 
 import org.bukkit.entity.Player
+import org.bukkit.inventory.meta.SkullMeta
 import java.util.*
 
 fun Player.applySkin(from: String) = getMojangProfile(from).applySkinToPlayer(this)
 
-fun Player.applySkin(from: UUID) = getMojangProfile(from).applySkinToPlayer(this)
+fun Player.applySkin(from: UUID) = applySkin("$from")
 
 fun Player.resetSkin() = getMojangProfile(name).applySkinToPlayer(this)
+
+fun SkullMeta.applySkin(from: String, replaceName: Boolean = false) =
+    getMojangProfile(from).applySkinToSkullMeta(this, replaceName)
+
+fun SkullMeta.applySkin(from: UUID, replaceName: Boolean = false) =
+    applySkin("$from", replaceName)
