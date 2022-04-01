ead62872133d36b4323af278de74b08ed4ba5eb1
TheFruxz <cedricspitzer@outlook.de>
2022-04-01 09:33:36 +0200

    Feature: ArmorStand item get/set operation functions

--- JET-Paper/src/main/kotlin/de/jet/paper/extension/paper/entity/AmorStand.kt
+++ JET-Paper/src/main/kotlin/de/jet/paper/extension/paper/entity/AmorStand.kt
@@ -1,6 +1,11 @@
 package de.jet.paper.extension.paper.entity
 
+import de.jet.paper.extension.display.ui.itemStack
+import de.jet.paper.tool.display.item.Item
+import org.bukkit.Material
 import org.bukkit.entity.ArmorStand
+import org.bukkit.inventory.EquipmentSlot
+import org.bukkit.inventory.ItemStack
 
 /**
  * This var represents the [ArmorStand.hasGravity] and
@@ -13,3 +18,39 @@ var ArmorStand.gravity: Boolean
     set(value) {
         setGravity(value)
     }
+
+/**
+ * This function sets the item of the [ArmorStand] at the given [equipmentSlot].
+ * This function utilizes the [ArmorStand.setItem] function.
+ * @author Fruxz
+ * @since 1.0
+ */
+operator fun ArmorStand.set(equipmentSlot: EquipmentSlot, itemStack: ItemStack) =
+    setItem(equipmentSlot, itemStack)
+
+/**
+* This function sets the item of the [ArmorStand] at the given [equipmentSlot].
+* This function utilizes the [ArmorStand.setItem] function.
+* @author Fruxz
+* @since 1.0
+*/
+operator fun ArmorStand.set(equipmentSlot: EquipmentSlot, item: Item) =
+    setItem(equipmentSlot, item.produce())
+
+/**
+* This function sets the item of the [ArmorStand] at the given [equipmentSlot].
+* This function utilizes the [ArmorStand.setItem] function.
+* @author Fruxz
+* @since 1.0
+*/
+operator fun ArmorStand.set(equipmentSlot: EquipmentSlot, material: Material) =
+    setItem(equipmentSlot, material.itemStack)
+
+/**
+ * This function gets the item of the [ArmorStand] at the given [equipmentSlot].
+ * This function utilizes the [ArmorStand.getItem] function.
+ * @author Fruxz
+ * @since 1.0
+ */
+operator fun ArmorStand.get(equipmentSlot: EquipmentSlot): ItemStack =
+    getItem(equipmentSlot)
