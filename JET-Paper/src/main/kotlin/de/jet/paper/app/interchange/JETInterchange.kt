95d05995caade5978fc129444b3eec9f1795bf3f
TheFruxz <cedricspitzer@outlook.de>
2022-04-05 00:47:05 +0200

    Feature: transform old JET interchange into new structured interchange

--- JET-Paper/src/main/kotlin/de/jet/paper/app/interchange/JETInterchange.kt
+++ JET-Paper/src/main/kotlin/de/jet/paper/app/interchange/JETInterchange.kt
@@ -1,6 +1,7 @@
 package de.jet.paper.app.interchange
 
 import de.jet.jvm.extension.container.replace
+import de.jet.jvm.extension.container.replaceVariables
 import de.jet.paper.extension.display.BOLD
 import de.jet.paper.extension.display.GOLD
 import de.jet.paper.extension.display.GRAY
@@ -60,86 +61,39 @@ class JETInterchange : StructuredInterchange("jet", buildInterchangeStructure {
 
 		concludedExecution {
 
+			lang("interchange.internal.jet.version").replaceVariables(
+				"version" to "${vendor.description.version}@${vendor.description.apiVersion}"
+			).notification(GENERAL, executor).display()
 
+		}
+
+	}
+
+	branch {
+
+		addContent("website", "repository")
+
+		concludedExecution {
+
+			lang("interchange.internal.jet.host").replaceVariables(
+				"website" to vendor.description.website
+			).notification(GENERAL, executor).display()
+
+		}
+
+	}
+
+	branch {
+
+		addContent("ping")
+
+		concludedExecution {
+
+			lang("interchange.internal.jet.ping")
+				.notification(GENERAL, executor).display()
 
 		}
 
 	}
 
 })
-
-class dJETInterchange : Interchange(
-	label = "jet",
-	protectedAccess = false,
-	completion = buildInterchangeStructure {
-		branch {
-			addContent("version", "website", "repository", "ping")
-		}
-	}
-) {
-	override val execution = execution {
-
-		var success = true
-
-		when {
-
-			parameters.isEmpty() -> {
-
-				"${GOLD}JET ${GRAY}was developed by$YELLOW TheFruxz$GRAY, and other contributors of the repository:$GOLD$BOLD ${vendor.description.website}"
-					.notification(GENERAL, executor).display()
-
-				"${YELLOW}JET is running & developed with Kotlin (the programming Language) from JetBrains. Check out their work https://jetbrains.com or https://kotlinlang.org"
-					.message(executor).display()
-
-			}
-
-			parameters.size == 1 -> {
-
-				when (parameters.first().lowercase()) {
-
-					"version" -> {
-
-						lang("interchange.internal.jet.version")
-							.replace("[version]" to "${vendor.description.version}@${vendor.description.apiVersion}")
-							.notification(GENERAL, executor).display()
-
-					}
-
-					"website", "repository" -> {
-
-						lang("interchange.internal.jet.host")
-							.replace("[website]" to vendor.description.website)
-							.notification(GENERAL, executor).display()
-
-					}
-
-					"ping" -> {
-
-						lang("interchange.internal.jet.ping")
-							.notification(GENERAL, executor).display()
-
-					}
-
-					else -> {
-						success = false
-					}
-
-				}
-
-			}
-
-			else -> {
-
-				success = false
-
-			}
-
-		}
-
-		if (success)
-			SUCCESS
-		else
-			WRONG_USAGE
-
-	}
-}
