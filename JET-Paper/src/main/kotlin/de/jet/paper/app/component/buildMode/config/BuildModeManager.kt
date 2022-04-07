3ff620aa9dfe8844ce5fca057243903c057040fc
TheFruxz <cedricspitzer@outlook.de>
2022-04-08 01:35:52 +0200

    // WIP

---
+++ JET-Paper/src/main/kotlin/de/jet/paper/app/component/buildMode/config/BuildModeManager.kt
@@ -0,0 +1,30 @@
+package de.jet.paper.app.component.buildMode.config
+
+import de.jet.jvm.extension.data.fromJson
+import de.jet.jvm.extension.data.toJson
+import de.jet.jvm.extension.div
+import de.jet.jvm.extension.generateFileAndPath
+import de.jet.jvm.extension.tryOrNull
+import de.jet.jvm.tool.smart.identification.Identifiable
+import de.jet.paper.tool.data.JetYamlFile
+import kotlin.io.path.readText
+import kotlin.io.path.writeText
+
+object BuildModeManager {
+
+	private val path = JetYamlFile.componentPath(Identifiable.custom("JET:BuildMode")) / "config.json"
+
+	private var _state: BuildModeConfiguration? = null
+
+	var state: BuildModeConfiguration
+		get() = _state ?: tryOrNull { path.readText().fromJson() } ?: BuildModeConfiguration().also {
+			path.toFile().generateFileAndPath()
+			state = it
+		}
+		set(value) {
+			_state = value
+			path.toFile().generateFileAndPath()
+			path.writeText(value.toJson())
+		}
+
+}
