248174c451b823133d120f2f177ffe960d31baaf
TheFruxz <cedricspitzer@outlook.de>
2022-04-24 22:53:57 +0200

    Feature: add modern and returning async and sync functions

--- MoltenKT-Paper/src/main/kotlin/de/moltenKt/paper/extension/tasky/Task.kt
+++ MoltenKT-Paper/src/main/kotlin/de/moltenKt/paper/extension/tasky/Task.kt
@@ -7,7 +7,11 @@ import de.moltenKt.paper.structure.service.Service
 import de.moltenKt.paper.tool.timing.tasky.Tasky
 import de.moltenKt.paper.tool.timing.tasky.TemporalAdvice
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
+import kotlinx.coroutines.future.await
 import kotlinx.coroutines.launch
+import java.util.concurrent.CompletableFuture
 
 /**
  * Exceptions are caught!
@@ -36,6 +40,41 @@ fun sync(
 	serviceVendor: Identity<Service> = Identity("undefined"),
 	process: Tasky.() -> Unit,
 ) = Tasky.task(vendor, temporalAdvice, killAtError, onStart, onStop, onCrash, serviceVendor, process)
+
+/**
+ * This function creates a sync task using the [Tasky] system.
+ * Internally a [CompletableFuture] is used to create the delayed result.
+ * Because the [CompletableFuture.await] function uses the coroutine suspend
+ * feature, this function also utilizes the suspend function feature, so the
+ * result can be easily managed into your existing MoltenKT App.
+ * @param process The process to be executed.
+ * @return The direct result of the process.
+ * @author Fruxz
+ * @since 1.0
+ */
+suspend fun <T> asSync(process: Tasky.() -> T): T {
+	val output = CompletableFuture<T>()
+
+	sync {
+		output.complete(process())
+	}
+
+	return output.await()
+}
+
+/**
+ * This function creates an async context using the KotlinX Coroutines Library.
+ * Returns the result as a [Deferred] object, for multiple-line executions.
+ * Utilizes the [CoroutineScope.async] function, to create a safe environment.
+ * @param process The process to be executed.
+ * @return The result of the process as a [Deferred] object.
+ * @see CoroutineScope.async
+ * @see Deferred
+ * @see CoroutineScope
+ * @author Fruxz
+ * @since 1.0
+ */
+fun <T> asAsync(process: suspend CoroutineScope.() -> T): Deferred<T> = system.coroutineScope.async(block = process)
 
 /**
  * Exceptions are caught!
