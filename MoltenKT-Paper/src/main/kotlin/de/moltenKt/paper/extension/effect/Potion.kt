bb4399d5c1a60c3248c63377f84d62b92eb10770
TheFruxz <cedricspitzer@outlook.de>
2022-04-24 22:32:54 +0200

    Chore(docs): update potion effect documentation

--- MoltenKT-Paper/src/main/kotlin/de/moltenKt/paper/extension/effect/Potion.kt
+++ MoltenKT-Paper/src/main/kotlin/de/moltenKt/paper/extension/effect/Potion.kt
@@ -6,6 +6,19 @@ import org.bukkit.potion.PotionEffectType
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.seconds
 
+/**
+ * This class-object like function creates a [PotionEffect] with the given parameters, but has an already some default values.
+ * @param type The type of the [PotionEffect]
+ * @param durationTicks The duration of the [PotionEffect]
+ * @param amplifier The amplifier of the [PotionEffect]
+ * @param ambient Whether the [PotionEffect] is ambient or not
+ * @param particles Whether the [PotionEffect] has particles or not
+ * @param icon The icon of the [PotionEffect]
+ * @see Duration
+ * @return The created [PotionEffect]
+ * @author Fruxz
+ * @since 1.0
+ */
 fun PotionEffect(type: PotionEffectType, durationTicks: Int, amplifier: Int = 0, ambient: Boolean = true, particles: Boolean = true, icon: Boolean = true) =
     PotionEffect(
         /* type = */ type,
@@ -15,5 +28,20 @@ fun PotionEffect(type: PotionEffectType, durationTicks: Int, amplifier: Int = 0,
         /* particles = */ particles,
         /* icon = */ icon
     )
+
+/**
+ * This class-object like function creates a [PotionEffect] with the given parameters, but has an already some default values.
+ * It utilizes the kotlin [Duration] API to convert the given [duration] to the Minecraft ticks.
+ * @param type The type of the [PotionEffect]
+ * @param duration The duration of the [PotionEffect]
+ * @param amplifier The amplifier of the [PotionEffect]
+ * @param ambient Whether the [PotionEffect] is ambient or not
+ * @param particles Whether the [PotionEffect] has particles or not
+ * @param icon The icon of the [PotionEffect]
+ * @see Duration
+ * @return The created [PotionEffect]
+ * @author Fruxz
+ * @since 1.0
+ */
 fun PotionEffect(type: PotionEffectType, duration: Duration = 10.seconds, amplifier: Int = 0, ambient: Boolean = true, particles: Boolean = true, icon: Boolean = true) =
     PotionEffect(type, duration.inWholeMinecraftTicks.toInt(), amplifier, ambient, particles, icon)
