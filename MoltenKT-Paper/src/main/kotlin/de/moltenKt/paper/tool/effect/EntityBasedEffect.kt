9496ad43f7d53b1dbced1611c2d7e29a721906db
TheFruxz <cedricspitzer@outlook.de>
2022-04-25 09:31:07 +0200

    Fix: wrong documentation specification in EntityBasedEffect

--- MoltenKT-Paper/src/main/kotlin/de/moltenKt/paper/tool/effect/EntityBasedEffect.kt
+++ MoltenKT-Paper/src/main/kotlin/de/moltenKt/paper/tool/effect/EntityBasedEffect.kt
@@ -6,10 +6,10 @@ import org.bukkit.entity.Entity
 interface EntityBasedEffect {
 
 	/**
-	 * This function plays the given [SoundEffect] to every player
+	 * This function plays the given [EntityBasedEffect] to every player
 	 * specified in [entities]. Look at [SoundEffect] for more
-	 * enhanced possibilities.
-	 * @param entities The receivers of the sound played.
+	 * enhanced possibilities with sounds.
+	 * @param entities The receivers of the effect played.
 	 * @author Fruxz
 	 * @since 1.0
 	 * @see SoundEffect
