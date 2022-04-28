c2b39f6fbb8e5d6b70cfab0049af70044174f047
TheFruxz <cedricspitzer@outlook.de>
2022-04-28 20:52:11 +0200

    Feature: more enhanced CompletableFuture coroutines api

--- MoltenKT-Core/src/main/kotlin/de/moltenKt/core/extension/javaUtil/CompletableFuture.kt
+++ MoltenKT-Core/src/main/kotlin/de/moltenKt/core/extension/javaUtil/CompletableFuture.kt
@@ -1,7 +1,11 @@
 package de.moltenKt.core.extension.javaUtil
 
 import de.moltenKt.core.extension.tryOrNull
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import kotlinx.coroutines.withTimeoutOrNull
 import java.util.concurrent.CompletableFuture
+import kotlin.time.Duration
 
 /**
  * Returns the value of the [CompletableFuture] or if it throws
@@ -20,3 +24,53 @@ fun <T> CompletableFuture<T>.getOrNull() = tryOrNull { get() }
  * @since 1.0
  */
 fun <T> CompletableFuture<T>.getOrDefault(default: T) = getOrNull() ?: default
+
+/**
+ * This suspend function returns the result [T] of this [CompletableFuture],
+ * when it is completed. (Uses Kotlin-Coroutines system)
+ * @param T the type of the result
+ * @return the result
+ * @author Fruxz
+ * @since 1.0
+ * @see CompletableFuture.join
+ */
+suspend fun <T> CompletableFuture<T>.await() =
+	withContext(Dispatchers.Default) { this@await.join() }
+
+/**
+ * This suspend function returns the result [T] of this [CompletableFuture],
+ * when it is completed. (Uses Kotlin-Coroutines system).
+ * After it is completed, the [block] block is [apply]'d on the result.
+ * @param T the type of the result
+ * @param block the block to apply on the result
+ * @return the result
+ * @author Fruxz
+ * @since 1.0
+ */
+suspend fun <T> CompletableFuture<T>.await(block: (T) -> Unit) =
+	await().apply(block)
+
+/**
+ * This suspend function returns the result [T] of this [CompletableFuture],
+ * when it is completed. (Uses Kotlin-Coroutines system).
+ * If it is not completed after [timeout] it returns null.
+ * @param T the type of the result
+ * @param timeout the timeout
+ * @return the result or null
+ * @author Fruxz
+ * @since 1.0
+ */
+suspend fun <T> CompletableFuture<T>.await(timeout: Duration) = withTimeoutOrNull(timeout) { await() }
+
+/**
+ * This suspend function returns the result [T] of this [CompletableFuture],
+ * when it is completed. (Uses Kotlin-Coroutines system).
+ * If it is not completed after [timeout] the [block] block is [apply]'d on the result.
+ * @param T the type of the result
+ * @param timeout the timeout
+ * @param block the block to apply on the result
+ * @return the result or null
+ * @author Fruxz
+ * @since 1.0
+ */
+suspend fun <T> CompletableFuture<T>.await(timeout: Duration, block: (T) -> Unit) = withTimeoutOrNull(timeout) { await(block) }
