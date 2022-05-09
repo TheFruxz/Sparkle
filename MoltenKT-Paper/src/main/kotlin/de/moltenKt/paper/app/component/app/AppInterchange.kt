23fc76116724566821f436ef2faa2fbe236a40ab
TheFruxz <cedricspitzer@outlook.de>
2022-05-09 12:44:56 +0200

    WIP: add app interchange basic structure

--- MoltenKT-Paper/src/main/kotlin/de/moltenKt/paper/app/component/app/AppInterchange.kt
+++ MoltenKT-Paper/src/main/kotlin/de/moltenKt/paper/app/component/app/AppInterchange.kt
@@ -5,6 +5,15 @@ import de.moltenKt.paper.structure.command.completion.buildInterchangeStructure
 
 internal class AppInterchange : StructuredInterchange("app", buildInterchangeStructure {
 
+    /*
+    /app list
+    /app stop <app|app#component>
+    /app start <app|app#component>
+    /app restart <app|app#component>
+    /app cache <app> info|clear (<level>)
+    /app info <app|app#component>
+    */
+
     // TODO: 28.03.22 WIP
 
 })
