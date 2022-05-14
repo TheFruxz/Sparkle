de5868095fc9a9105936eceac1642b8e604ca1dc
TheFruxz <cedricspitzer@outlook.de>
2022-05-15 00:24:50 +0200

    Feature: add app interchange structure

--- MoltenKT-Paper/src/main/kotlin/de/moltenKt/paper/app/component/app/AppInterchange.kt
+++ MoltenKT-Paper/src/main/kotlin/de/moltenKt/paper/app/component/app/AppInterchange.kt
@@ -12,8 +12,91 @@ internal class AppInterchange : StructuredInterchange("app", buildInterchangeStr
     /app restart <app|app#component>
     /app cache <app> info|clear (<level>)
     /app info <app|app#component>
+
+    /app list
+    /app at/@ <app|component> start/stop/restart/cache/inf
     */
 
-    // TODO: 28.03.22 WIP
+    branch {
+
+        addContent("list")
+
+        concludedExecution {
+
+        }
+
+    }
+
+    branch {
+
+        addContent("at", "@")
+
+        branch {
+
+            addContent("start")
+
+            concludedExecution {
+
+            }
+
+        }
+
+        branch {
+
+            addContent("stop")
+
+            concludedExecution {
+
+            }
+
+        }
+
+        branch {
+
+            addContent("restart")
+
+            concludedExecution {
+
+            }
+
+        }
+
+        branch {
+
+            addContent("cache")
+
+            branch {
+
+                addContent("info")
+
+                concludedExecution {
+
+                }
+
+            }
+
+            branch {
+
+                addContent("clear")
+
+                concludedExecution {
+
+                }
+
+            }
+
+        }
+
+        branch {
+
+            addContent("info")
+
+            concludedExecution {
+
+            }
+
+        }
+
+    }
 
 })
