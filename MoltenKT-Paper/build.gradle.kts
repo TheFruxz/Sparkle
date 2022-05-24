d48ed583a05283a16a2ca21281df4fc80843fc2c
TheFruxz <cedricspitzer@outlook.de>
2022-05-24 18:35:44 +0200

    Chore(dependency): update kotlinx serialization to 1.3.3

--- MoltenKT-Paper/build.gradle.kts
+++ MoltenKT-Paper/build.gradle.kts
@@ -58,7 +58,7 @@ dependencies {
 
     @Suppress("DependencyOnStdlib") shadow(kotlin("stdlib"))
     shadow(kotlin("reflect"))
-    shadow("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
+    shadow("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
     shadow("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
     shadow("org.slf4j:slf4j-api:2.0.0-alpha7")
     shadow("org.jetbrains.exposed:exposed-core:0.38.2")
