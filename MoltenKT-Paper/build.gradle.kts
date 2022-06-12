9bd5bdf22e6301d481c693a7af943df853cc9172
TheFruxz <cedricspitzer@outlook.de>
2022-06-12 22:20:32 +0200

    ⚙️ Update ktor and coroutines dependency version

--- MoltenKT-Paper/build.gradle.kts
+++ MoltenKT-Paper/build.gradle.kts
@@ -61,13 +61,13 @@ dependencies {
     @Suppress("DependencyOnStdlib") shadow(kotlin("stdlib"))
     shadow(kotlin("reflect"))
     shadow("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
-    shadow("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
+    shadow("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.2")
     shadow("org.slf4j:slf4j-api:2.0.0-alpha7")
     shadow("org.jetbrains.exposed:exposed-core:0.38.2")
     shadow("org.jetbrains.exposed:exposed-dao:0.38.2")
     shadow("org.jetbrains.exposed:exposed-jdbc:0.38.2")
    
-    shadow("io.ktor:ktor-client-cio:2.0.1")
+    shadow("io.ktor:ktor-client-cio:2.0.2")
     shadow("io.ktor:ktor-client-core-jvm:2.0.2") // Ktor
     shadow("io.ktor:ktor-serialization-kotlinx-json:2.0.2")
 
