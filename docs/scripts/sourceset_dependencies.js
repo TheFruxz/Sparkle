24009ca3926669e50eaaf6e102a1d8bd53e8ea66
TheFruxz <cedricspitzer@outlook.de>
2022-06-15 21:09:03 +0200

    📑 Update docs pages

--- docs/scripts/sourceset_dependencies.js
+++ docs/scripts/sourceset_dependencies.js
@@ -1,1 +1,1 @@
-sourceset_dependencies = '{":MoltenKT-Unfold:dokkaHtmlPartial/main":[],":MoltenKT-JavaCord:dokkaHtmlPartial/main":[],":MoltenKT-Paper:dokkaHtmlPartial/main":[],":MoltenKT-Core:dokkaHtmlPartial/main":[]}'
+sourceset_dependencies = '{":MoltenKT-Unfold:dokkaHtmlPartial/main":[],":MoltenKT-Paper:dokkaHtmlPartial/main":[],":MoltenKT-Core:dokkaHtmlPartial/main":[]}'
