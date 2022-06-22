25c8b9d208b22fb2247440e4043513ae9345c64e
TheFruxz <cedricspitzer@outlook.de>
2022-06-22 09:58:52 +0200

    💬 Change name of generic type in completion trace specification

--- MoltenKT-Paper/src/main/kotlin/de/moltenKt/paper/structure/command/completion/tracing/CompletionTraceResult.kt
+++ MoltenKT-Paper/src/main/kotlin/de/moltenKt/paper/structure/command/completion/tracing/CompletionTraceResult.kt
@@ -5,12 +5,12 @@ import de.moltenKt.paper.extension.interchange.InterchangeExecutor
 import de.moltenKt.paper.structure.command.completion.InterchangeStructure
 import de.moltenKt.paper.structure.command.completion.tracing.CompletionTraceResult.Conclusion.*
 
-data class CompletionTraceResult<EXECUTOR : InterchangeExecutor, T : TreeBranch<*, *, *>>(
-	val waysMatching: List<PossibleTraceWay<T>>, // the possible ways to execute the trace
-	val waysIncomplete: List<PossibleTraceWay<T>>, // the possible, but uncompleted ways to execute the trace - more input is needed to complete the trace
-	val waysOverflow: List<PossibleTraceWay<T>>, // the arguments are too many, but the whole trace way path matches to the input
-	val waysFailed: List<PossibleTraceWay<T>>, // the ways that died during the trace and cannot be used with the query
-	val waysNoDestination: List<PossibleTraceWay<T>>, // the ways that have no destination, but the whole trace way path matches to the input
+data class CompletionTraceResult<EXECUTOR : InterchangeExecutor, BRANCH : TreeBranch<*, *, *>>(
+	val waysMatching: List<PossibleTraceWay<BRANCH>>, // the possible ways to execute the trace
+	val waysIncomplete: List<PossibleTraceWay<BRANCH>>, // the possible, but uncompleted ways to execute the trace - more input is needed to complete the trace
+	val waysOverflow: List<PossibleTraceWay<BRANCH>>, // the arguments are too many, but the whole trace way path matches to the input
+	val waysFailed: List<PossibleTraceWay<BRANCH>>, // the ways that died during the trace and cannot be used with the query
+	val waysNoDestination: List<PossibleTraceWay<BRANCH>>, // the ways that have no destination, but the whole trace way path matches to the input
 	val traceBase: InterchangeStructure<EXECUTOR>, // the base of the trace
 	val executedQuery: List<String>, // the original query, that was used to execute the trace
 ) {
@@ -20,12 +20,8 @@ data class CompletionTraceResult<EXECUTOR : InterchangeExecutor, T : TreeBranch<
 	} else if (waysMatching.isEmpty()) {
 		if (waysIncomplete.isEmpty() && waysOverflow.isEmpty() && waysFailed.isEmpty() && waysNoDestination.isEmpty()) {
 			EMPTY
-		} else {
-			NO_RESULT
-		}
-	} else {
-		MULTIPLE_RESULTS
-	}
+		} else NO_RESULT
+	} else MULTIPLE_RESULTS
 
 	enum class Conclusion {
 
