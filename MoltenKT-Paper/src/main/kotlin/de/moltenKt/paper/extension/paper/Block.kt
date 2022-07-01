eb1f43d70aa6ca21dea79eb844f1049303c1e6c3
TheFruxz <cedricspitzer@outlook.de>
2022-07-01 22:54:19 +0200

    📦 Improve container api

--- MoltenKT-Paper/src/main/kotlin/de/moltenKt/paper/extension/paper/Block.kt
+++ MoltenKT-Paper/src/main/kotlin/de/moltenKt/paper/extension/paper/Block.kt
@@ -1,7 +1,68 @@
 package de.moltenKt.paper.extension.paper
 
+import org.bukkit.block.Block
 import org.bukkit.block.BlockState
+import org.bukkit.block.Container
 import org.bukkit.block.Sign
+import org.bukkit.inventory.Inventory
 
+/**
+ * This function casts a [BlockState] to the correct,
+ * editable block state.
+ * @author Fruxz
+ * @since 1.0
+ */
 val BlockState.sign: Sign
     get() = this as Sign
+
+/**
+ * This function takes the sign's state and applies the
+ * [builder] process to it.
+ * @author Fruxz
+ * @since 1.0
+ */
+fun Block.editSign(builder: Sign.() -> Unit) {
+    state.sign.apply(builder).update()
+}
+
+/**
+ * This function returns the inventory of the given Block,
+ * or null, if the block itself does not hold any inventory.
+ * @author Fruxz
+ * @since 1.0
+ */
+val Block.inventoryOrNull: Inventory?
+    get() = if (this is Container) inventory else null
+
+/**
+ * This function returns the inventory of the given Block,
+ * or throws an [NoSuchElementException], if the block itself
+ * does not hold any inventory.
+ * @throws NoSuchElementException if the block itself does not hold any inventory.
+ * @author Fruxz
+ * @since 1.0
+ */
+val Block.inventory: Inventory
+    get() = this.inventoryOrNull ?: throw NoSuchElementException("Block has no container")
+
+/**
+ * This function returns a snapshot inventory of the given Block,
+ * or null, if the block itself does not hold any inventory.
+ * @see Container.getSnapshotInventory
+ * @author Fruxz
+ * @since 1.0
+ */
+val Block.inventorySnapshotOrNull: Inventory?
+    get() = if (this is Container) snapshotInventory else null
+
+/**
+ * This function returns a snapshot inventory of the given Block,
+ * or throws an [NoSuchElementException], if the block itself
+ * does not hold any inventory.
+ * @throws NoSuchElementException if the block itself does not hold any inventory.
+ * @see Container.getSnapshotInventory
+ * @author Fruxz
+ * @since 1.0
+ */
+val Block.inventorySnapshot: Inventory
+    get() = this.inventorySnapshotOrNull ?: throw NoSuchElementException("Block has no container")
