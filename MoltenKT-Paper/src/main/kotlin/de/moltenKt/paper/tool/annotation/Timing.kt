8a43db676074f0b83af6b40ee1f47c86de0c7820
TheFruxz <cedricspitzer@outlook.de>
2022-07-05 16:22:41 +0200

    🧑‍💻add some annotations for future
    These annotations should help, to easily understand the problems with the executed code, if some problems with sync & async appear

---
+++ MoltenKT-Paper/src/main/kotlin/de/moltenKt/paper/tool/annotation/Timing.kt
@@ -0,0 +1,33 @@
+package de.moltenKt.paper.tool.annotation
+
+/**
+ * This code is starting a new sync runner
+ * **NOTE, that this annotation is optional and only a helping indicator!**
+ * @author Fruxz
+ * @since 1.0
+ */
+annotation class RunningSync
+
+/**
+ * This code is starting a new async runner
+ * **NOTE, that this annotation is optional and only a helping indicator!**
+ * @author Fruxz
+ * @since 1.0
+ */
+annotation class RunningAsync
+
+/**
+ * This code requires, to run inside a sync environment
+ * **NOTE, that this annotation is optional and only a helping indicator!**
+ * @author Fruxz
+ * @since 1.0
+ */
+annotation class RequiresSync
+
+/**
+ * This code requires, to run inside a async environment
+ * **NOTE, that this annotation is optional and only a helping indicator!**
+ * @author Fruxz
+ * @since 1.0
+ */
+annotation class RequiresAsync
