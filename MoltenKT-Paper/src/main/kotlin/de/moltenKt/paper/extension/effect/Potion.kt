ce641de2a8008fa4038dcd41855a1460569e3dc9
TheFruxz <cedricspitzer@outlook.de>
2022-07-09 15:17:38 +0200

    🧪 add potion effect builder function

--- MoltenKT-Paper/src/main/kotlin/de/moltenKt/paper/extension/effect/Potion.kt
+++ MoltenKT-Paper/src/main/kotlin/de/moltenKt/paper/extension/effect/Potion.kt
@@ -45,3 +45,20 @@ fun PotionEffect(type: PotionEffectType, durationTicks: Int, amplifier: Int = 0,
  */
 fun PotionEffect(type: PotionEffectType, duration: Duration = 10.seconds, amplifier: Int = 0, ambient: Boolean = true, particles: Boolean = true, icon: Boolean = true) =
     PotionEffect(type, duration.inWholeMinecraftTicks.toInt(), amplifier, ambient, particles, icon)
+
+/**
+ * This function creates a [PotionEffect] with the given parameters, but has an already some default values.
+ * This function also applies the [builder] to the created [PotionEffect] and returns the modified result.
+ * @param type The type of the [PotionEffect]
+ * @param duration The duration of the [PotionEffect]
+ * @param amplifier The amplifier of the [PotionEffect]
+ * @param ambient Whether the [PotionEffect] is ambient or not
+ * @param particles Whether the [PotionEffect] has particles or not
+ * @param icon The icon of the [PotionEffect]
+ * @see Duration
+ * @return The created [PotionEffect]
+ * @author Fruxz
+ * @since 1.0
+ */
+fun buildPotionEffect(type: PotionEffectType, duration: Duration = 10.seconds, amplifier: Int = 0, ambient: Boolean = true, particles: Boolean = true, icon: Boolean = true, builder: PotionEffect.() -> Unit) =
+    PotionEffect(type, duration, amplifier, ambient, particles, icon).apply(builder)
