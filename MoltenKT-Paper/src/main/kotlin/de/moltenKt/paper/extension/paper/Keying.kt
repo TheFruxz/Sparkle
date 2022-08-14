09a8b0e9cc3cb2d1de6bbebf05980bd2af21120c
TheFruxz <cedricspitzer@outlook.de>
2022-08-14 23:16:13 +0200

    feature: add some object-based keys
    Add keys for players, locations and blocks

--- MoltenKT-Paper/src/main/kotlin/de/moltenKt/paper/extension/paper/Keying.kt
+++ MoltenKT-Paper/src/main/kotlin/de/moltenKt/paper/extension/paper/Keying.kt
@@ -2,7 +2,11 @@ package de.moltenKt.paper.extension.paper
 
 import de.moltenKt.unfold.extension.KeyingStrategy
 import net.kyori.adventure.key.Key
+import org.bukkit.Location
 import org.bukkit.NamespacedKey
+import org.bukkit.World
+import org.bukkit.block.Block
+import org.bukkit.entity.Player
 
 fun NamespacedKey.subKey(value: String, strategy: KeyingStrategy = KeyingStrategy.CONTINUE): NamespacedKey = NamespacedKey(
 	when (strategy) {
@@ -21,3 +25,12 @@ fun Key.subNamespacedKey(value: String, strategy: KeyingStrategy = KeyingStrateg
 	},
 	value
 )
+
+val Player.key: Key
+	get() = Key.key("$uniqueId")
+
+val Location.key: Key
+	get() = Key.key("location_${world.uid}.$x.$y.$z")
+
+val Block.key: Key
+	get() = Key.key("block_${world.uid}.$x.$y.$z")
