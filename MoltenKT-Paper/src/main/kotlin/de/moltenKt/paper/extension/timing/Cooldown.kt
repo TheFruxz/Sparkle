e7d9caacb83f800461c0c11ca2a9f7e2c1e1fda6
TheFruxz <cedricspitzer@outlook.de>
2022-08-14 23:41:49 +0200

    feature: more enhanced cooldown output and filter

--- MoltenKT-Paper/src/main/kotlin/de/moltenKt/paper/extension/timing/Cooldown.kt
+++ MoltenKT-Paper/src/main/kotlin/de/moltenKt/paper/extension/timing/Cooldown.kt
@@ -7,6 +7,7 @@ import org.bukkit.Location
 import org.bukkit.block.Block
 import org.bukkit.entity.Player
 import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
 
 // Player cooldown
 
@@ -17,10 +18,17 @@ fun Player.setCooldown(key: Any, duration: Duration) =
 	StaticCooldown.setCooldown(this.key to key, Calendar.now() + duration)
 
 fun Player.getCooldown(key: Any) =
-	StaticCooldown.getCooldown(this.key to key)
+	StaticCooldown.getCooldown(this.key to key)?.let { destination ->
+		RunningCooldown(this.key to key, destination)
+	}
 
 fun Player.hasCooldown(key: Any) =
 	StaticCooldown.hasCooldown(this.key to key)
+
+val Player.activeCooldowns: List<RunningCooldown>
+	get() = StaticCooldown.cooldownStats
+		.filter { it.key.let { key -> key is Pair<*, *> && key.first == this } }
+		.map { RunningCooldown(it.key, it.value) }
 
 // Block cooldown
 
@@ -31,10 +39,17 @@ fun Block.setCooldown(key: Any, duration: Duration) =
 	StaticCooldown.setCooldown(this.key to key, Calendar.now() + duration)
 
 fun Block.getCooldown(key: Any) =
-	StaticCooldown.getCooldown(this.key to key)
+	StaticCooldown.getCooldown(this.key to key)?.let { destination ->
+		RunningCooldown(this.key to key, destination)
+	}
 
 fun Block.hasCooldown(key: Any) =
 	StaticCooldown.hasCooldown(this.key to key)
+
+val Block.activeCooldowns: List<RunningCooldown>
+	get() = StaticCooldown.cooldownStats
+		.filter { it.key.let { key -> key is Pair<*, *> && key.first == this } }
+		.map { RunningCooldown(it.key, it.value) }
 
 // Location cooldown
 
@@ -45,9 +60,18 @@ fun Location.setCooldown(key: Any, duration: Duration) =
 	StaticCooldown.setCooldown(this.key to key, Calendar.now() + duration)
 
 fun Location.getCooldown(key: Any) =
-	StaticCooldown.getCooldown(this.key to key)
+	StaticCooldown.getCooldown(this.key to key)?.let { destination ->
+		RunningCooldown(this.key to key, destination)
+	}
 
 fun Location.hasCooldown(key: Any) =
 	StaticCooldown.hasCooldown(this.key to key)
 
+val Location.activeCooldowns: List<RunningCooldown>
+	get() = StaticCooldown.cooldownStats
+		.filter { it.key.let { key -> key is Pair<*, *> && key.first == this } }
+		.map { RunningCooldown(it.key, it.value) }
 
+data class RunningCooldown constructor(val key: Any, val destination: Calendar) {
+	val remaining = (destination.timeInMilliseconds - Calendar.now().timeInMilliseconds).milliseconds
+}
