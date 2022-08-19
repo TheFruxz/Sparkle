662ca5e32bab7ff9e2b9293f038cc02e8456a05e
TheFruxz <cedricspitzer@outlook.de>
2022-08-19 11:44:53 +0200

    chore: deprecate component builder on paper side

--- MoltenKT-Paper/src/main/kotlin/de/moltenKt/paper/extension/display/Component.kt
+++ MoltenKT-Paper/src/main/kotlin/de/moltenKt/paper/extension/display/Component.kt
@@ -1,7 +1,9 @@
 package de.moltenKt.paper.extension.display
 
+import de.moltenKt.unfold.text
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.TextComponent.Builder
 
+@Deprecated("Use the unfold buildComponent/text instead", ReplaceWith("text(base, process)", "de.moltenKt.unfold.text"))
 fun buildTextComponent(base: Builder = Component.text("").toBuilder(), process: Builder.() -> Unit) =
-	base.apply(process).build()
+	text(base, process)
