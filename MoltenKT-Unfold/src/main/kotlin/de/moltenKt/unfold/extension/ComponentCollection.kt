7d1a67dee5250b902839cae5cbd3be1cd00538d7
TheFruxz <cedricspitzer@outlook.de>
2022-08-20 10:42:36 +0200

    feature: component line split

--- MoltenKT-Unfold/src/main/kotlin/de/moltenKt/unfold/extension/ComponentCollection.kt
+++ MoltenKT-Unfold/src/main/kotlin/de/moltenKt/unfold/extension/ComponentCollection.kt
@@ -9,3 +9,16 @@ fun Collection<ComponentLike>.joinToComponent(): TextComponent {
     forEach { builder.append(it) }
     return builder
 }
+
+/**
+ * This function splits every line of the component to its
+ * own component, using [asStyledString] conversion, then
+ * [split] and after that the back conversion via [asStyledComponent].
+ * This is useful if you need the specific lines of the component,
+ * like adding a prefix to each line.
+ * @return A list of components, each representing a line of the original component
+ * @author Fruxz
+ * @since 1.0
+ */
+fun <T : ComponentLike> T.lines(): List<TextComponent> =
+    asStyledString.split("<br>").asStyledComponents
