fcf840a2c76dab06707dacf5950edde59fb5ee66
TheFruxz <cedricspitzer@outlook.de>
2022-08-24 17:32:15 +0200

    feature: allow quick replacing with regex

--- MoltenKT-Unfold/src/main/kotlin/de/moltenKt/unfold/extension/Replace.kt
+++ MoltenKT-Unfold/src/main/kotlin/de/moltenKt/unfold/extension/Replace.kt
@@ -1,5 +1,6 @@
 package de.moltenKt.unfold.extension
 
+import de.moltenKt.unfold.buildComponent
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.ComponentLike
 import java.util.regex.Pattern
@@ -31,6 +32,35 @@ fun <T : Component> T.replace(key: String, value: String): Component =
 fun <T : Component> T.replace(key: String, value: ComponentLike): Component =
 	replaceText {
 		it.match(key)
+		it.replacement(value)
+	}
+
+/**
+ * Replaces all occurrences of the given [regex] with the given [value]
+ * and then returns the result component.
+ * @param regex The regex to replace.
+ * @param value The value to replace the regex with.
+ * @return The result component.
+ * @author Fruxz
+ * @since 1.0
+ */
+fun <T : Component> T.replace(regex: Regex, value: String): Component =
+	replaceText {
+		it.match(Pattern.compile(regex.pattern))
+		it.replacement(value)
+	}
+
+/**
+ * Replaces all occurrences of the given [regex] with the given [value]
+ * and then returns the result component.
+ * @param regex The regex to replace.
+ * @param value The value to replace the regex with.
+ * @return The result component.
+ * @author Fruxz
+ */
+fun <T : Component> T.replace(regex: Regex, value: ComponentLike): Component =
+	replaceText {
+		it.match(Pattern.compile(regex.pattern))
 		it.replacement(value)
 	}
 
