a7626ccd51adc189e8eb38cb9f839b58aa23049c
TheFruxz <cedricspitzer@outlook.de>
2022-09-13 17:14:16 +0200

    chore: upgrade issued interchange to non-lang system components

--- MoltenKT-Paper/src/main/kotlin/de/moltenKt/paper/structure/app/interchange/IssuedInterchange.kt
+++ MoltenKT-Paper/src/main/kotlin/de/moltenKt/paper/structure/app/interchange/IssuedInterchange.kt
@@ -1,12 +1,19 @@
 package de.moltenKt.paper.structure.app.interchange
 
 import de.moltenKt.paper.extension.display.notification
-import de.moltenKt.paper.extension.lang
 import de.moltenKt.paper.structure.command.Interchange
 import de.moltenKt.paper.structure.command.InterchangeResult.SUCCESS
 import de.moltenKt.paper.structure.command.completion.emptyInterchangeStructure
 import de.moltenKt.paper.structure.command.execution
 import de.moltenKt.paper.tool.display.message.Transmission.Level.ERROR
+import de.moltenKt.unfold.extension.dyeGray
+import de.moltenKt.unfold.extension.dyeRed
+import de.moltenKt.unfold.extension.dyeYellow
+import de.moltenKt.unfold.extension.style
+import de.moltenKt.unfold.plus
+import de.moltenKt.unfold.text
+import net.kyori.adventure.text.format.NamedTextColor
+import net.kyori.adventure.text.format.TextDecoration.BOLD
 
 class IssuedInterchange(
 	label: String,
@@ -20,9 +27,14 @@ class IssuedInterchange(
 
 	override val execution = execution {
 
-		lang["interchange.structure.issue.register"]
-			.notification(ERROR, executor)
-			.display()
+		text {
+			this + text("Oops!").style(NamedTextColor.RED, BOLD)
+			this + text(" This interchange ").dyeGray()
+			this + text("crashed").dyeRed()
+			this + text(" during the ").dyeGray()
+			this + text("registration").dyeYellow()
+			this + text(" process, please report this to a technician!").dyeGray()
+		}.notification(ERROR, executor).display()
 
 		SUCCESS
 	}
