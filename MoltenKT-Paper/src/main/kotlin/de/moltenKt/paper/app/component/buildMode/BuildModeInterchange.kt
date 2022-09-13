d963f67b1cff0e46ceb41b807b4e63bf52784264
TheFruxz <cedricspitzer@outlook.de>
2022-09-13 11:44:08 +0200

    chore: upgrade build mode interchange to non-lang system components

--- MoltenKT-Paper/src/main/kotlin/de/moltenKt/paper/app/component/buildMode/BuildModeInterchange.kt
+++ MoltenKT-Paper/src/main/kotlin/de/moltenKt/paper/app/component/buildMode/BuildModeInterchange.kt
@@ -1,28 +1,36 @@
 package de.moltenKt.paper.app.component.buildMode
 
 import de.moltenKt.core.extension.container.page
-import de.moltenKt.core.extension.container.replaceVariables
 import de.moltenKt.core.extension.container.toUUID
 import de.moltenKt.core.extension.math.ceilToInt
 import de.moltenKt.core.extension.switchResult
 import de.moltenKt.paper.Constants
 import de.moltenKt.paper.app.MoltenCache
+import de.moltenKt.paper.extension.display.BOLD
 import de.moltenKt.paper.extension.display.notification
 import de.moltenKt.paper.extension.interchange.InterchangeExecutor
-import de.moltenKt.paper.extension.lang
 import de.moltenKt.paper.extension.paper.buildMode
 import de.moltenKt.paper.extension.paper.offlinePlayer
 import de.moltenKt.paper.extension.paper.onlinePlayers
 import de.moltenKt.paper.structure.command.InterchangeResult.SUCCESS
 import de.moltenKt.paper.structure.command.InterchangeResult.WRONG_USAGE
-import de.moltenKt.paper.structure.command.structured.StructuredInterchange
 import de.moltenKt.paper.structure.command.completion.InterchangeStructureInputRestriction
 import de.moltenKt.paper.structure.command.completion.buildInterchangeStructure
 import de.moltenKt.paper.structure.command.completion.component.CompletionAsset
 import de.moltenKt.paper.structure.command.completion.component.CompletionComponent
 import de.moltenKt.paper.structure.command.completion.ignoreCase
 import de.moltenKt.paper.structure.command.completion.isNotRequired
+import de.moltenKt.paper.structure.command.structured.StructuredInterchange
 import de.moltenKt.paper.tool.display.message.Transmission.Level.*
+import de.moltenKt.unfold.extension.dyeGold
+import de.moltenKt.unfold.extension.dyeGray
+import de.moltenKt.unfold.extension.dyeGreen
+import de.moltenKt.unfold.extension.dyeRed
+import de.moltenKt.unfold.extension.dyeYellow
+import de.moltenKt.unfold.extension.style
+import de.moltenKt.unfold.plus
+import de.moltenKt.unfold.text
+import net.kyori.adventure.text.Component
 import org.bukkit.OfflinePlayer
 
 internal class BuildModeInterchange : StructuredInterchange(
@@ -33,25 +41,57 @@ internal class BuildModeInterchange : StructuredInterchange(
 		fun enableBuildMode(executor: InterchangeExecutor, target: OfflinePlayer) {
 			if (!target.buildMode) {
 				target.buildMode = true
-				lang["interchange.internal.buildmode.enable"]
-					.replaceVariables("player" to target.name)
-					.notification(APPLIED, executor).display()
-			} else
-				lang["interchange.internal.buildmode.stay"]
-					.replaceVariables("player" to target.name)
-					.notification(FAIL, executor).display()
+
+				text {
+					this + text("The ").dyeGray()
+					this + text("Build-Mode").dyeGold()
+					this + text(" of the Player '").dyeGray()
+					this + text(target.name ?: "Unknown").dyeYellow()
+					this + text("' has been ").dyeGray()
+					this + text("enabled").dyeGreen()
+					this + text("!").dyeGray()
+				}.notification(APPLIED, executor).display()
+
+			} else {
+				text {
+					this + text("The ").dyeGray()
+					this + text("Build-Mode").dyeGold()
+					this + text(" of the Player '").dyeGray()
+					this + text(target.name ?: "Unknown").dyeYellow()
+					this + text("' is already ").dyeGray()
+					this + text("enabled").dyeGreen()
+					this + text("!").dyeGray()
+				}.notification(FAIL, executor).display()
+			}
 		}
 
 		fun disableBuildMode(executor: InterchangeExecutor, target: OfflinePlayer) {
 			if (target.buildMode) {
 				target.buildMode = false
-				lang["interchange.internal.buildmode.disable"]
-					.replaceVariables("player" to target.name)
-					.notification(APPLIED, executor).display()
-			} else
-				lang["interchange.internal.buildmode.stay"]
-					.replaceVariables("player" to target.name)
-					.notification(FAIL, executor).display()
+
+				text {
+					this + text("The ").dyeGray()
+					this + text("Build-Mode").dyeGold()
+					this + text(" of the Player '").dyeGray()
+					this + text(target.name ?: "Unknown").dyeYellow()
+					this + text("' has been ").dyeGray()
+					this + text("disabled").dyeGreen()
+					this + text("!").dyeGray()
+				}.notification(APPLIED, executor).display()
+
+			} else {
+
+				text {
+					this + text("The ").dyeGray()
+					this + text("Build-Mode").dyeGold()
+					this + text(" of the Player '").dyeGray()
+					this + text(target.name ?: "Unknown").dyeYellow()
+					this + text("' is already ").dyeGray()
+					this + text("disabled").dyeGreen()
+					this + text("!").dyeGray()
+				}.notification(FAIL, executor).display()
+
+			}
 		}
 
 		fun toggleBuildMode(executor: InterchangeExecutor, target: OfflinePlayer) {
@@ -72,29 +112,32 @@ internal class BuildModeInterchange : StructuredInterchange(
 				val pageValue = MoltenCache.buildModePlayers.page(page, Constants.ENTRIES_PER_PAGE)
 
 				if (pageValue.content.isNotEmpty()) {
-					buildString {
 
-						appendLine(
-							lang["interchange.internal.buildmode.list.header"].replaceVariables(
-								"p1" to pageValue.page + 1,
-								"p2" to pageValue.pages,
-							)
-						)
+					text {
+						this + text("These players have Build-Mode enabled: ").dyeGray()
+						this + text("(Page ${pageValue.page + 1} of ${pageValue.pages})").dyeYellow()
 
 						pageValue.content.withIndex().forEach {
 							val player = offlinePlayer(it.value.identity.toUUID())
 
-							lang["interchange.internal.buildmode.list.line"].replaceVariables(
-								"player" to player.name,
-								"statusColor" to player.isOnline.switchResult("<green>", "<gray>")
-							).let(::appendLine)
+							this + Component.newline()
+							this + text(">- ").dyeGray()
+							this + text(player.name ?: "Unknown").style(BOLD).let { output ->
+								if (player.isOnline) output.dyeGreen() else output.dyeGray()
+							}
 
 						}
-
 					}.notification(INFO, executor).display()
-				} else
-					lang["interchange.internal.buildmode.list.empty"]
-						.notification(FAIL, executor).display()
+
+				} else {
+
+					text {
+						this + text("There are currently ").dyeGray()
+						this + text("no players").dyeRed()
+						this + text(" with Build-Mode enabled!").dyeGray()
+					}.notification(FAIL, executor).display()
+
+				}
 			}
 
 			concludedExecution {
@@ -230,10 +273,13 @@ internal class BuildModeInterchange : StructuredInterchange(
 					concludedExecution {
 						val target = getInput(1, InterchangeStructureInputRestriction.OFFLINE_PLAYER)
 
-						lang["interchange.internal.buildmode.info"].replaceVariables(
-							"state" to target.buildMode.switchResult("<green><bold>enabled</bold>", "<red><bold>disabled</bold>"),
-							"player" to target.name
-						).notification(INFO, executor).display()
+						text {
+							this + text("Build mode of the Player '").dyeGray()
+							this + text(target.name ?: "Unknown").dyeYellow()
+							this + text("' is ").dyeGray()
+							this + text(target.buildMode.switchResult("enabled", "disabled")).dyeGreen()
+							this + text("!").dyeGray()
+						}.notification(INFO, executor).display()
 
 					}
 
