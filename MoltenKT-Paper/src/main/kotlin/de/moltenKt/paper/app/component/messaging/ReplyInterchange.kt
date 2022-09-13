51c3a43f5f3ccafb46d728d57f2cb181a3cd58cc
TheFruxz <cedricspitzer@outlook.de>
2022-09-13 16:47:07 +0200

    chore: upgrade reply interchange to non-lang system components

--- MoltenKT-Paper/src/main/kotlin/de/moltenKt/paper/app/component/messaging/ReplyInterchange.kt
+++ MoltenKT-Paper/src/main/kotlin/de/moltenKt/paper/app/component/messaging/ReplyInterchange.kt
@@ -1,12 +1,15 @@
 package de.moltenKt.paper.app.component.messaging
 
 import de.moltenKt.paper.extension.display.notification
-import de.moltenKt.paper.extension.lang
 import de.moltenKt.paper.structure.command.InterchangeUserRestriction
-import de.moltenKt.paper.structure.command.structured.StructuredInterchange
 import de.moltenKt.paper.structure.command.completion.buildInterchangeStructure
 import de.moltenKt.paper.structure.command.completion.infiniteSubParameters
+import de.moltenKt.paper.structure.command.structured.StructuredInterchange
 import de.moltenKt.paper.tool.display.message.Transmission
+import de.moltenKt.unfold.extension.dyeGray
+import de.moltenKt.unfold.extension.dyeRed
+import de.moltenKt.unfold.plus
+import de.moltenKt.unfold.text
 import org.bukkit.entity.Player
 
 internal class ReplyInterchange : StructuredInterchange("reply", userRestriction = InterchangeUserRestriction.ONLY_PLAYERS, structure = buildInterchangeStructure {
@@ -21,7 +24,11 @@ internal class ReplyInterchange : StructuredInterchange("reply", userRestriction
 
             if (!Messaging.sendReply(player, parameters.joinToString(" "))) {
 
-                lang["interchange.internal.message.reply.failed.noChatReceiver"].notification(Transmission.Level.FAIL, executor).display()
+                text {
+                    this + text("You're").dyeGray()
+                    this + text(" not ").dyeRed()
+                    this + text("having an active conversation with someone online!").dyeGray()
+                }.notification(Transmission.Level.FAIL, executor).display()
 
             }
 
