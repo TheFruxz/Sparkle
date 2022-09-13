9f333de4077f76d07b4f8d597646e71b69b54571
TheFruxz <cedricspitzer@outlook.de>
2022-09-13 16:46:43 +0200

    chore: upgrade marking interchange to non-lang system components

--- MoltenKT-Paper/src/main/kotlin/de/moltenKt/paper/app/component/marking/MarkingInterchange.kt
+++ MoltenKT-Paper/src/main/kotlin/de/moltenKt/paper/app/component/marking/MarkingInterchange.kt
@@ -2,12 +2,15 @@ package de.moltenKt.paper.app.component.marking
 
 import de.moltenKt.paper.extension.display.notification
 import de.moltenKt.paper.extension.display.ui.addItems
-import de.moltenKt.paper.extension.lang
 import de.moltenKt.paper.extension.tasky.asSync
 import de.moltenKt.paper.structure.command.completion.buildInterchangeStructure
 import de.moltenKt.paper.structure.command.completion.ignoreCase
 import de.moltenKt.paper.structure.command.structured.StructuredPlayerInterchange
 import de.moltenKt.paper.tool.display.message.Transmission.Level.APPLIED
+import de.moltenKt.unfold.extension.dyeGold
+import de.moltenKt.unfold.extension.dyeGray
+import de.moltenKt.unfold.plus
+import de.moltenKt.unfold.text
 
 internal class MarkingInterchange : StructuredPlayerInterchange(
 	label = "markings",
@@ -23,8 +26,11 @@ internal class MarkingInterchange : StructuredPlayerInterchange(
 
 				asSync { executor.inventory.addItems(MarkingComponent.markingItem) }
 
-				lang["component.markingTool.interchange.success"]
-					.notification(APPLIED, executor).display()
+				text {
+					this + text("The ").dyeGray()
+					this + text("Marking Tool").dyeGold()
+					this + text(" has been added to your inventory.").dyeGray()
+				}.notification(APPLIED, executor).display()
 
 			}
 
