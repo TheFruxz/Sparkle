acb9a244189ab5a519880f465dbaa65986168c4c
TheFruxz <cedricspitzer@outlook.de>
2022-10-06 22:25:13 +0200

    fix: RequiresComponent annotation now only targets classes

--- src/main/kotlin/de/fruxz/sparkle/framework/annotation/RequiresComponent.kt
+++ src/main/kotlin/de/fruxz/sparkle/framework/annotation/RequiresComponent.kt
@@ -5,4 +5,5 @@ import kotlin.reflect.KClass
 
 @Repeatable
 @MustBeDocumented
+@Target(AnnotationTarget.CLASS)
 annotation class RequiresComponent(val module: KClass<out Component>) // TODO add to start blocks of interchanges, components and more, so that they can check if the given module is loaded
