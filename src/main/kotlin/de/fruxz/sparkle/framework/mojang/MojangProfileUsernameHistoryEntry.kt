f6109ee333be6f939d9e93d0802f676847b07da8
TheFruxz <cedricspitzer@outlook.de>
2022-10-08 10:56:45 +0200

    fix: opt-in for experimental serialization api

--- src/main/kotlin/de/fruxz/sparkle/framework/mojang/MojangProfileUsernameHistoryEntry.kt
+++ src/main/kotlin/de/fruxz/sparkle/framework/mojang/MojangProfileUsernameHistoryEntry.kt
@@ -1,12 +1,14 @@
 package de.fruxz.sparkle.framework.mojang
 
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.JsonNames
 
 @Serializable
 @SerialName("MojangProfileUsernameHistoryEntry")
-data class MojangProfileUsernameHistoryEntry(
+@OptIn(ExperimentalSerializationApi::class)
+data class MojangProfileUsernameHistoryEntry constructor(
     @JsonNames("changed_at") val changedAt: String = "empty",
     val username: String
 )
