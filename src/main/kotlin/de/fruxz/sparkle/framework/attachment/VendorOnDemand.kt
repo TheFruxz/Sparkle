4de3f4d537f2cc1d268b6f5be987a1f0b00e4150
TheFruxz <cedricspitzer@outlook.de>
2022-11-13 01:17:24 +0100

    chore: add on-demand vendor documentation

--- src/main/kotlin/de/fruxz/sparkle/framework/attachment/VendorOnDemand.kt
+++ src/main/kotlin/de/fruxz/sparkle/framework/attachment/VendorOnDemand.kt
@@ -4,6 +4,12 @@ import de.fruxz.sparkle.framework.infrastructure.app.App
 
 interface VendorOnDemand {
 
+	/**
+	 * The class, which implements this [VendorOnDemand] interfaces
+	 * selects the [App] on runtime. This property is here, if you
+	 * want to 'force' the class, to take your [App] instead of the
+	 * automatically selected one.
+	 */
 	val preferredVendor: App?
 
 	fun replaceVendor(newVendor: App, override: Boolean = false): Boolean
