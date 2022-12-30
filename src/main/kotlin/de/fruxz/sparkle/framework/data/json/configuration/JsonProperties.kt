038439b2a5ad566b0b0678c03420291b81832506
TheFruxz <cedricspitzer@outlook.de>
2022-12-30 16:58:24 +0100

    chore: adapt to new property system

--- src/main/kotlin/de/fruxz/sparkle/framework/data/json/configuration/JsonProperties.kt
+++ src/main/kotlin/de/fruxz/sparkle/framework/data/json/configuration/JsonProperties.kt
@@ -1,22 +1,26 @@
 package de.fruxz.sparkle.framework.data.json.configuration
 
-import de.fruxz.ascend.extension.div
-import de.fruxz.ascend.tool.delegate.jsonProperty
+import de.fruxz.ascend.annotation.LanguageFeature
+import de.fruxz.ascend.tool.delegate.JsonProperty
+import de.fruxz.ascend.tool.delegate.property
 import de.fruxz.sparkle.framework.data.file.SparklePath
 import de.fruxz.sparkle.framework.infrastructure.component.Component
 import de.fruxz.sparkle.server.SparkleApp
 import java.nio.file.Path
+import kotlin.io.path.div
 
+@LanguageFeature
 fun <T : Any> property(
 	app: SparkleApp,
 	key: String,
 	file: Path = SparklePath.appPath(app) / "configuration.json",
 	defaultValue: () -> T
-) = jsonProperty(file, key, defaultValue)
+): JsonProperty<T> = property(file, key, defaultValue)
 
+@LanguageFeature
 fun <T : Any> property(
 	component: Component,
 	key: String,
 	file: Path = SparklePath.componentPath(component) / "configuration.json",
 	defaultValue: () -> T,
-) = jsonProperty(file, key, defaultValue)
+): JsonProperty<T> = property(file, key, defaultValue)
