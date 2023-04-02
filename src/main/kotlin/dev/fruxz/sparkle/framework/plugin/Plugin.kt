71aaed42d7b06ba72f68f7c23da6b69702bb05fa
TheFruxz <cedricspitzer@outlook.de>
2023-04-02 19:26:34 +0200

    feature: improved plugin extensions

--- src/main/kotlin/dev/fruxz/sparkle/framework/plugin/Plugin.kt
+++ src/main/kotlin/dev/fruxz/sparkle/framework/plugin/Plugin.kt
@@ -2,8 +2,9 @@ package dev.fruxz.sparkle.framework.plugin
 
 import dev.fruxz.sparkle.framework.SparklePlugin
 import dev.fruxz.sparkle.framework.marker.SparkleDSL
+import net.kyori.adventure.key.Key
 import org.bukkit.Bukkit
-import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.plugin.Plugin
 
 @SparkleDSL
 fun SparklePlugin(plugin: SparklePlugin.() -> Unit) = object : SparklePlugin(plugin) { }
@@ -16,13 +17,17 @@ val pluginManager by lazy { Bukkit.getPluginManager() }
  * This function accesses the plugin instance of the given plugin [T] or null if it doesn't exist.
  */
 @SparkleDSL
-inline fun <reified T : JavaPlugin> instanceOrNull() = pluginManager.plugins.firstOrNull { it is T } as T?
+inline fun <reified T : Plugin> instanceOrNull() = pluginManager.plugins.firstOrNull { it is T } as T?
 
 /**
  * This function accesses the plugin instance of the given plugin [T] or throws an exception if it doesn't exist.
  */
 @Throws(NoSuchElementException::class)
 @SparkleDSL
-inline fun <reified T : JavaPlugin> instance() = instanceOrNull<T>() ?: throw NoSuchElementException("Plugin ${T::class.simpleName} not found!")
+inline fun <reified T : Plugin> instance() = instanceOrNull<T>() ?: throw NoSuchElementException("Plugin ${T::class.simpleName} not found!")
+
+// Key
+
+fun Plugin.key() = Key.key(this.name)
 
 
