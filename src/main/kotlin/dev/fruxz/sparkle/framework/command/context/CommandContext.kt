0b44fb6ee3975bf6f83d65190ffbb5210803ebec
TheFruxz <cedricspitzer@outlook.de>
2023-04-09 18:46:07 +0200

    feature: add reply functions to CommandContext

--- src/main/kotlin/dev/fruxz/sparkle/framework/command/context/CommandContext.kt
+++ src/main/kotlin/dev/fruxz/sparkle/framework/command/context/CommandContext.kt
@@ -1,6 +1,8 @@
 package dev.fruxz.sparkle.framework.command.context
 
+import de.fruxz.stacked.text
 import dev.fruxz.sparkle.framework.command.sparkle.CommandBranch
+import net.kyori.adventure.text.Component
 import org.bukkit.command.CommandSender
 import org.bukkit.command.Command as BukkitCommand
 
@@ -14,6 +16,12 @@ interface CommandContext {
 
     val parameters: List<String>
 
+    // interaction
+    fun reply(text: String) = executor.sendMessage(text(text))
+
+    fun reply(message: Component) = executor.sendMessage(message)
+
+    // transformation
     fun asBranchContext(branch: CommandBranch, branchInput: List<String>): BranchExecutionContext {
         return BranchExecutionContext(
             executor = executor,
