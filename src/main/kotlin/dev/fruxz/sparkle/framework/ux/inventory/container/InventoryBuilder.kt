9c80193869c9aaae1ecbcf54d718ae245d2f53ac
TheFruxz <cedricspitzer@outlook.de>
2023-05-02 22:07:38 +0200

    feature: add buildInventory(...) functions

--- src/main/kotlin/dev/fruxz/sparkle/framework/ux/inventory/container/InventoryBuilder.kt
+++ src/main/kotlin/dev/fruxz/sparkle/framework/ux/inventory/container/InventoryBuilder.kt
@@ -2,6 +2,7 @@ package dev.fruxz.sparkle.framework.ux.inventory.container
 
 import net.kyori.adventure.text.ComponentLike
 import org.bukkit.Bukkit
+import org.bukkit.event.inventory.InventoryType
 import org.bukkit.inventory.Inventory
 import org.bukkit.inventory.InventoryHolder
 
@@ -10,5 +11,11 @@ fun buildInventory(size: Int, owner: InventoryHolder? = null, process: Inventory
 
 fun buildInventory(size: Int, label: ComponentLike, owner: InventoryHolder? = null, process: Inventory.() -> Unit = { }) =
     Bukkit.createInventory(owner, size, label.asComponent()).apply(process)
+
+fun buildInventory(type: InventoryType, owner: InventoryHolder? = null, process: Inventory.() -> Unit = { }) =
+    Bukkit.createInventory(owner, type).apply(process)
+
+fun buildInventory(type: InventoryType, label: ComponentLike, owner: InventoryHolder? = null, process: Inventory.() -> Unit = { }) =
+    Bukkit.createInventory(owner, type, label.asComponent()).apply(process)
 
 // TODO buildInventory with canvas base
