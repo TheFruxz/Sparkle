a775110b2f7b861b4e4f6bf59b7e444bbbf35bed
TheFruxz <cedricspitzer@outlook.de>
2023-05-02 19:05:58 +0200

    chore: add ktor, serialization & coroutines to plugin loader dependencies

--- src/main/kotlin/dev/fruxz/sparkle/server/LocalSparkleLoader.java
+++ src/main/kotlin/dev/fruxz/sparkle/server/LocalSparkleLoader.java
@@ -24,7 +24,13 @@ public class LocalSparkleLoader implements PluginLoader {
             "org.jetbrains.kotlin:kotlin-reflect:1.8.21",
             "com.github.TheFruxz:Ascend:ba6c401235",
             "com.github.TheFruxz:Stacked:74e454b253",
-            "com.github.TheFruxz:Kojang:1.0-RC2"
+            "com.github.TheFruxz:Kojang:1.0-RC2",
+            "io.ktor:ktor-client-cio:2.3.0",
+            "io.ktor:ktor-client-core-jvm:2.3.0",
+            "io.ktor:ktor-serialization-kotlinx-json:2.3.0",
+            "io.ktor:ktor-client-content-negotiation:2.3.0",
+            "org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0",
+            "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4"
     );
 
     @Override
