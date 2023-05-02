bf55b7e37e1992f5774ced52ed179427dcd441e0
TheFruxz <cedricspitzer@outlook.de>
2023-05-02 22:05:55 +0200

    feature: add caching to sparkle http client

--- src/main/kotlin/dev/fruxz/sparkle/framework/network/SparkleHTTP.kt
+++ src/main/kotlin/dev/fruxz/sparkle/framework/network/SparkleHTTP.kt
@@ -3,12 +3,13 @@ package dev.fruxz.sparkle.framework.network
 import dev.fruxz.ascend.json.globalJson
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.cache.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.serialization.kotlinx.json.*
 
 val sparkleHttpClient by lazy {
     HttpClient(CIO) {
-        // TODO enable http cache option
+        install(HttpCache)
         install(ContentNegotiation) {
             json(globalJson)
         }
