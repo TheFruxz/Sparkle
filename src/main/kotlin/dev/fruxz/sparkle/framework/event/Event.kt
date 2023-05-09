76d2658a88666dda488ae2123d8563a135a3a7a1
TheFruxz <cedricspitzer@outlook.de>
2023-05-09 23:12:48 +0200

    feature: add unregisterEvents extension functions

--- src/main/kotlin/dev/fruxz/sparkle/framework/event/Event.kt
+++ src/main/kotlin/dev/fruxz/sparkle/framework/event/Event.kt
@@ -2,9 +2,13 @@ package dev.fruxz.sparkle.framework.event
 
 import org.bukkit.Location
 import org.bukkit.entity.Player
+import org.bukkit.event.HandlerList
+import org.bukkit.event.Listener
 import org.bukkit.event.block.Action
 import org.bukkit.event.inventory.InventoryInteractEvent
 import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.plugin.Plugin
+import org.bukkit.plugin.PluginManager
 
 val Action.isPhysical: Boolean
     get() = this == Action.PHYSICAL
@@ -19,3 +23,11 @@ val PlayerInteractEvent.possibleFutureBlock: Location?
 
 val <T : InventoryInteractEvent> T.player: Player
     get() = whoClicked as Player
+
+// management
+
+fun PluginManager.unregisterEvents() = HandlerList.unregisterAll()
+
+fun PluginManager.unregisterEvents(plugin: Plugin) = HandlerList.unregisterAll(plugin)
+
+fun PluginManager.unregisterEvents(listener: Listener) = HandlerList.unregisterAll(listener)
