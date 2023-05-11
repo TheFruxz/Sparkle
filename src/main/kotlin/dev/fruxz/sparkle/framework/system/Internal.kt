8b163cef5e8b36bff4af8e72a1a2ac08d5b0bb5f
TheFruxz <cedricspitzer@outlook.de>
2023-05-11 14:07:35 +0200

    chore: add api internal and kdoc to internalSyncCommandws function
    Because this access a highly risky code of paper, which are currently under a rework.
    This may break in the future, so this have to be used with caution

--- src/main/kotlin/dev/fruxz/sparkle/framework/system/Internal.kt
+++ src/main/kotlin/dev/fruxz/sparkle/framework/system/Internal.kt
@@ -5,6 +5,14 @@ import org.jetbrains.annotations.ApiStatus
 import kotlin.reflect.full.declaredFunctions
 import kotlin.reflect.jvm.isAccessible
 
+/**
+ * It is dangerous to use this api, because it depends on a function
+ * inside the code of the paper server, which is currently under
+ * affected by heavy changes, like the brigadier support.
+ * @author Fruxz
+ * @since 2023.1
+ */
+@ApiStatus.Internal
 fun Server.internalSyncCommands() {
     this::class.declaredFunctions.firstOrNull { it.name == "syncCommands" }?.let {
         it.isAccessible = true
