276d254194e42e4d75a6348b52f865d0695e4bae
TheFruxz <cedricspitzer@outlook.de>
2023-05-14 22:54:47 +0200

    fix: transmission extension functions do not respect multiple liens

--- src/main/kotlin/dev/fruxz/sparkle/framework/ux/messaging/Message.kt
+++ src/main/kotlin/dev/fruxz/sparkle/framework/ux/messaging/Message.kt
@@ -1,26 +1,33 @@
 package dev.fruxz.sparkle.framework.ux.messaging
 
 import dev.fruxz.stacked.extension.asStyledComponents
+import dev.fruxz.stacked.extension.lines
 import net.kyori.adventure.text.ComponentLike
 import org.bukkit.command.CommandSender
 
 // Messages
 
 fun List<ComponentLike>.transmission(vararg participants: CommandSender) =
-    Transmission(participants = participants.toSet(), content = map { it.asComponent() })
+    Transmission(participants = participants.toSet(), content = flatMap { it.lines })
 
 fun ComponentLike.transmission(vararg participants: CommandSender) =
-    listOf(this).transmission(*participants)
+    this.lines.transmission(*participants)
+
+infix fun ComponentLike.transmission(participant: CommandSender) =
+    this.lines.transmission(participant)
 
 fun String.transmission(vararg participants: CommandSender) =
     asStyledComponents.transmission(*participants)
 
+infix fun String.transmission(participant: CommandSender) =
+    asStyledComponents.transmission(participant)
+
 // Notification
 
 fun List<ComponentLike>.transmission(vararg participants: CommandSender, theme: Transmission.Theme?) =
-    Transmission(participants = participants.toSet(), content = map { it.asComponent() }, theme = theme)
+    Transmission(participants = participants.toSet(), content = flatMap { it.lines }, theme = theme)
 
 fun ComponentLike.transmission(vararg participants: CommandSender, theme: Transmission.Theme?) =
-    listOf(this).transmission(*participants, theme = theme)
+    this.lines.transmission(*participants, theme = theme)
 fun String.transmission(vararg participants: CommandSender, theme: Transmission.Theme?) =
     asStyledComponents.transmission(*participants, theme = theme)
