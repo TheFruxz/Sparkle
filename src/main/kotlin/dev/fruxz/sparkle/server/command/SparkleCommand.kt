1b2d5f4024efcb1f904b654bca3f4da0d0d68c11
TheFruxz <cedricspitzer@outlook.de>
2023-05-25 17:12:59 +0200

    chore: remove debug stuff

--- src/main/kotlin/dev/fruxz/sparkle/server/command/SparkleCommand.kt
+++ src/main/kotlin/dev/fruxz/sparkle/server/command/SparkleCommand.kt
@@ -3,48 +3,18 @@ package dev.fruxz.sparkle.server.command
 import dev.fruxz.sparkle.framework.command.annotations.Description
 import dev.fruxz.sparkle.framework.command.annotations.Label
 import dev.fruxz.sparkle.framework.command.annotations.Usage
-import dev.fruxz.sparkle.framework.command.registerCommand
 import dev.fruxz.sparkle.framework.command.sparkle.BranchContent
 import dev.fruxz.sparkle.framework.command.sparkle.Command
-import dev.fruxz.sparkle.framework.command.unregisterCommand
-import dev.fruxz.sparkle.framework.coroutine.task.delayed
-import dev.fruxz.sparkle.framework.coroutine.task.doSync
-import dev.fruxz.sparkle.framework.plugin.plugin
 import dev.fruxz.sparkle.framework.system.sparklePrefix
 import dev.fruxz.sparkle.framework.ux.messaging.transmission
 import dev.fruxz.sparkle.server.LocalSparklePlugin
 import org.bukkit.entity.Entity
-import kotlin.time.Duration.Companion.seconds
 
 @Label("sparkle")
 @Description("A command for everything sparkle")
 @Usage("/sparkle debug <on|off>")
 internal class SparkleCommand : Command() {
     override fun configure() {
-
-        branch {
-            content("test")
-            execution {
-
-                reply("<gold>action!")
-
-                plugin<LocalSparklePlugin>().getCommand("sparkle")?.let {
-                    println("removed")
-                    it.let {
-                        plugin<LocalSparklePlugin>().unregisterCommand(it)
-                        delayed(10.seconds) {
-                            doSync {
-                                println("re-registered")
-                                plugin<LocalSparklePlugin>().registerCommand(SparkleCommand(), SparkleCommand::class)
-                            }
-                        }
-                    }
-                } ?: println("not found")
-
-                reply("<green>test")
-
-            }
-        }
 
         branch {
 
