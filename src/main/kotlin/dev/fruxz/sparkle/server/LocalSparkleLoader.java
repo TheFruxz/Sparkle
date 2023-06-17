961711de67a496642084ff19f94f6d35c7847988
TheFruxz <cedricspitzer@outlook.de>
2023-06-17 20:58:38 +0200

    fix: update dependency versions in loader

--- src/main/kotlin/dev/fruxz/sparkle/server/LocalSparkleLoader.java
+++ src/main/kotlin/dev/fruxz/sparkle/server/LocalSparkleLoader.java
@@ -20,17 +20,17 @@ public class LocalSparkleLoader implements PluginLoader {
      * The list of dependencies, which this loader should load.
      */
     public static List<String> dependencies = List.of(
-            "org.jetbrains.kotlin:kotlin-stdlib:1.8.21",
-            "org.jetbrains.kotlin:kotlin-reflect:1.8.21",
+            "org.jetbrains.kotlin:kotlin-stdlib:1.8.22",
+            "org.jetbrains.kotlin:kotlin-reflect:1.8.22",
             "com.github.TheFruxz:Ascend:119069b785",
             "com.github.TheFruxz:Stacked:74e454b253",
             "com.github.TheFruxz:Kojang:1.0-RC2",
-            "io.ktor:ktor-client-cio:2.3.0",
-            "io.ktor:ktor-client-core-jvm:2.3.0",
-            "io.ktor:ktor-serialization-kotlinx-json:2.3.0",
-            "io.ktor:ktor-client-content-negotiation:2.3.0",
-            "org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0",
-            "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4"
+            "io.ktor:ktor-client-cio:2.3.1",
+            "io.ktor:ktor-client-core-jvm:2.3.1",
+            "io.ktor:ktor-serialization-kotlinx-json:2.3.1",
+            "io.ktor:ktor-client-content-negotiation:2.3.1",
+            "org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1",
+            "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1"
     );
 
     @Override
