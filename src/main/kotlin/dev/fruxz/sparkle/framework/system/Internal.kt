0b981c9616633c78f06a1694e80411dedf3df3f1
TheFruxz <cedricspitzer@outlook.de>
2023-06-17 20:58:15 +0200

    chore: support async use of internalSyncCommands function

--- src/main/kotlin/dev/fruxz/sparkle/framework/system/Internal.kt
+++ src/main/kotlin/dev/fruxz/sparkle/framework/system/Internal.kt
@@ -1,5 +1,7 @@
 package dev.fruxz.sparkle.framework.system
 
+import dev.fruxz.sparkle.framework.coroutine.task.doSync
+import org.bukkit.Bukkit
 import org.bukkit.Server
 import org.jetbrains.annotations.ApiStatus
 import kotlin.reflect.full.declaredFunctions
@@ -7,15 +9,17 @@ import kotlin.reflect.jvm.isAccessible
 
 /**
  * It is dangerous to use this api, because it depends on a function
- * inside the code of the paper server, which is currently under
+ * inside the code of the paper server, which is currently
  * affected by heavy changes, like the brigadier support.
  * @author Fruxz
  * @since 2023.1
  */
 @ApiStatus.Internal
 fun Server.internalSyncCommands() {
-    this::class.declaredFunctions.firstOrNull { it.name == "syncCommands" }?.let {
-        it.isAccessible = true
-        it.call(this)
-    }
+    {
+        this::class.declaredFunctions.firstOrNull { it.name == "syncCommands" }?.let {
+            it.isAccessible = true
+            it.call(this)
+        }
+    }.let { process -> if (Bukkit.isPrimaryThread()) process.invoke() else doSync { process.invoke() } }
 }
