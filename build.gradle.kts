5d2d6626730185b3d9d34a4c178283f562b04ca4
Fruxz <cedricspitzer@outlook.de>
2023-09-17 20:35:13 +0200

    chore: update kotlin to 1.9.20-beta

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.9.0"
+    kotlin("jvm") version "1.9.20-Beta"
     kotlin("plugin.serialization") version "1.9.0"
     id("io.papermc.paperweight.userdev") version "1.5.5"
     `maven-publish`
