93053d3eecc44cc1a8532a2009e0634c04a21343
TheFruxz <cedricspitzer@outlook.de>
2023-09-20 00:19:02 +0200

    chore: import biome

--- src/main/kotlin/dev/fruxz/sparkle/framework/entity/Entity.kt
+++ src/main/kotlin/dev/fruxz/sparkle/framework/entity/Entity.kt
@@ -1,10 +1,11 @@
 package dev.fruxz.sparkle.framework.entity
 
+import org.bukkit.block.Biome
 import org.bukkit.entity.Entity
 
 var Entity.gravity: Boolean
     get() = hasGravity()
     set(value) = setGravity(value)
 
-val Entity.biome: org.bukkit.block.Biome
+val Entity.biome: Biome
     get() = location.block.biome
