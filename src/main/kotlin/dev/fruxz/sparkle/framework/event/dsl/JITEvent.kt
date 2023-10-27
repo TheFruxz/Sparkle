c2938eb0bfaacac3c29932b82e0eb7ac04142328
TheFruxz <cedricspitzer@outlook.de>
2023-10-27 20:55:25 +0200

    feature: include event class in JITEvents

--- src/main/kotlin/dev/fruxz/sparkle/framework/event/dsl/JITEvent.kt
+++ src/main/kotlin/dev/fruxz/sparkle/framework/event/dsl/JITEvent.kt
@@ -9,8 +9,10 @@ import org.bukkit.event.EventPriority
 import org.bukkit.event.entity.EntityEvent
 import org.bukkit.event.player.PlayerEvent
 import org.bukkit.plugin.Plugin
+import kotlin.reflect.KClass
 
 abstract class JITEvent<T : Event>(
+    val eventClazz: KClass<T>,
     val priority: EventPriority = EventPriority.NORMAL,
     val ignoreCancelled: Boolean = false,
     val plugin: Plugin = sparkle,
@@ -20,21 +22,23 @@ abstract class JITEvent<T : Event>(
 }
 
 abstract class JITPlayerEvent<T : PlayerEvent>(
+    eventClazz: KClass<T>,
     priority: EventPriority = EventPriority.NORMAL,
     ignoreCancelled: Boolean = false,
     val autoRemoval: Boolean = true,
     plugin: Plugin = sparkle,
-) : JITEvent<T>(priority, ignoreCancelled, plugin) {
+) : JITEvent<T>(eventClazz, priority, ignoreCancelled, plugin) {
     abstract operator fun invoke(event: T, player: Player)
     override fun invoke(event: T) = invoke(event, event.player)
 }
 
 abstract class JITEntityEvent<T : EntityEvent>(
+    eventClazz: KClass<T>,
     priority: EventPriority = EventPriority.NORMAL,
     ignoreCancelled: Boolean = false,
     val autoRemoval: Boolean = true,
     plugin: Plugin = sparkle,
-) : JITEvent<T>(priority, ignoreCancelled, plugin) {
+) : JITEvent<T>(eventClazz, priority, ignoreCancelled, plugin) {
     abstract operator fun invoke(event: T, entity: Entity)
     override fun invoke(event: T) = invoke(event, event.entity)
 }
