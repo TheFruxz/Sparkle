d0a32e484eddca1ccbe7fb06433fdad84dcc610a
TheFruxz <cedricspitzer@outlook.de>
2024-04-01 05:23:29 +0200

    fix: missing fruxz.dev dependency in plugin loader

--- src/main/kotlin/dev/fruxz/sparkle/server/LocalSparkleLoader.java
+++ src/main/kotlin/dev/fruxz/sparkle/server/LocalSparkleLoader.java
@@ -30,6 +30,7 @@ public class LocalSparkleLoader implements PluginLoader {
         maven.addRepository(new RemoteRepository.Builder("jitpack", "default", "https://jitpack.io/").build());
         maven.addRepository(new RemoteRepository.Builder("central", "default", "https://repo.maven.apache.org/maven2/").build());
         maven.addRepository(new RemoteRepository.Builder("minecraft", "default", "https://libraries.minecraft.net").build());
+        maven.addRepository(new RemoteRepository.Builder("fruxz", "default", "https://repo.fruxz.dev/releases").build());
 
         classpathBuilder.addLibrary(maven);
     }
