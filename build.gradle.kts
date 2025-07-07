6b0845c1e541dd451f433bf892f0a18e0cb469bd
TheFruxz <cedricspitzer@outlook.de>
2025-07-07 23:30:38 +0200

    chore: gradle cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import io.papermc.paperweight.util.path
-
 plugins {
     kotlin("jvm") version "2.2.0"
     kotlin("plugin.serialization") version "2.2.0"
@@ -9,9 +7,19 @@ plugins {
 }
 
 var host = "github.com/TheFruxz/Sparkle"
+val includedDependencies = mutableListOf<String>()
 
 version = "2025.7-dev"
 group = "dev.fruxz"
+
+@DslMarker
+annotation class DependencyDSL
+
+@DependencyDSL
+fun Dependency?.include() = this?.apply {
+    val computedVersion = version ?: kotlin.coreLibrariesVersion
+    includedDependencies += "${group}:${name}:${computedVersion}"
+}
 
 repositories {
 
@@ -27,26 +35,19 @@ repositories {
 
 }
 
-val includedDependencies = mutableListOf<String>()
-
-fun Dependency?.deliver() = this?.apply {
-    val computedVersion = version ?: kotlin.coreLibrariesVersion
-    includedDependencies += "${group}:${name}:${computedVersion}"
-}
-
 dependencies {
 
     // Internal
 
-    api("dev.fruxz:ascend:2025.7-8af65e5").deliver()
-    api("dev.fruxz:stacked:2025.5-3733615").deliver()
-    api("dev.fruxz:brigadikt:2025.4-48276a1-preview").deliver()
-    api("dev.fruxz:kojang:1.1.2").deliver()
+    api("dev.fruxz:ascend:2025.7-8af65e5").include()
+    api("dev.fruxz:stacked:2025.5-3733615").include()
+    api("dev.fruxz:brigadikt:2025.4-48276a1-preview").include()
+    api("dev.fruxz:kojang:1.1.2").include()
 
     // Kotlin
 
-    implementation(kotlin("stdlib")).deliver()
-    implementation(kotlin("reflect")).deliver()
+    implementation(kotlin("stdlib")).include()
+    implementation(kotlin("reflect")).include()
     testImplementation(kotlin("test"))
 
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
@@ -57,24 +58,16 @@ dependencies {
     paperweight.paperDevBundle("1.21.7-R0.1-SNAPSHOT")
     implementation("com.mojang:brigadier:1.0.18")
 
-    implementation("io.ktor:ktor-client-cio:3.1.1").deliver()
-    implementation("io.ktor:ktor-client-core-jvm:3.1.1").deliver()
-    implementation("io.ktor:ktor-serialization-kotlinx-json:3.1.1").deliver()
-    implementation("io.ktor:ktor-client-content-negotiation:3.1.1").deliver()
+    implementation("io.ktor:ktor-client-cio:3.1.1").include()
+    implementation("io.ktor:ktor-client-core-jvm:3.1.1").include()
+    implementation("io.ktor:ktor-serialization-kotlinx-json:3.1.1").include()
+    implementation("io.ktor:ktor-client-content-negotiation:3.1.1").include()
 
 }
 
 tasks {
 
-    val generateDependenciesFile = register("generateDependenciesFile") {
-        doLast {
-            val outputFile = file("src/main/resources/dependencies.txt")
-            outputFile.writeText(includedDependencies.joinToString("\n"))
-        }
-    }
-
     processResources {
-        dependsOn(generateDependenciesFile)
         expand(
             "version" to project.version,
             "name" to project.name,
@@ -88,6 +81,33 @@ tasks {
 
     runServer {
         this.minecraftVersion("1.21.7")
+    }
+
+    val generateRepositoriesFile = register("generateRepositoriesFile") {
+        val outputFile = file(layout.buildDirectory.dir("generated/resources/repositories.list"))
+        doLast {
+            outputFile.parentFile.mkdirs()
+            outputFile.writeText(repositories.filterIsInstance<MavenArtifactRepository>().map { it.url }.joinToString("\n"))
+        }
+    }
+
+    val generateDependenciesFile = register("generateDependenciesFile") {
+        val outputFile = file(layout.buildDirectory.dir("generated/resources/dependencies.list"))
+        doLast {
+            outputFile.parentFile.mkdirs()
+            outputFile.writeText(includedDependencies.joinToString("\n"))
+        }
+    }
+
+    jar {
+        dependsOn(generateRepositoriesFile)
+        dependsOn(generateDependenciesFile)
+        from(layout.buildDirectory.dir("generated/resources/dependencies.list")) {
+            into("") // Top-Level der JAR
+        }
+        from(layout.buildDirectory.dir("generated/resources/repositories.list")) {
+            into("") // Top-Level der JAR
+        }
     }
 
 }
